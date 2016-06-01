9aca4367302a7e3b216fbb341dc5d67ba617d839
menzies <menzies@jpl.nasa.gov>
2016-05-31 18:18:55 -0700

    [+] Add pass through converter [+] Add unit tests for image converters

--- Imaging/Serializers/ImageConverters.cs
+++ Imaging/Serializers/ImageConverters.cs
@@ -12,8 +12,9 @@ namespace OPS.Imaging
     {
         public static IImageConverter ValueRangeToNormalizedImage = new ValueRange2NormalizedImage();
         public static IImageConverter NormalizedImageToValueRange = new NormalizedImage2ValueRange();
+        public static IImageConverter PassThrough = new ValuePassThrough();
 
-                
+
         private class ValueRange2NormalizedImage : IImageConverter
         {
             /// <summary>
@@ -21,7 +22,7 @@ namespace OPS.Imaging
             /// Assumes input values range from 0-MaxValue for most types
             /// No scaling is performed on float or double types
             /// </summary>
-            /// <typeparam name="T"></typeparam>
+            /// <typeparam name="T">Type used to determine the input value range</typeparam>
             /// <param name="image"></param>
             /// <returns></returns>
             public Image Convert<T>(Image image)
@@ -56,13 +57,16 @@ namespace OPS.Imaging
             }
         }
 
-        /// <summary>
-        /// Returns a copy of an image with values ranging from 0 to T.MaxValue
-        /// Assumes input image values are normalized 0-1 for most types
-        /// No scaling is performed on float or double types
-        /// </summary>
         private class NormalizedImage2ValueRange : IImageConverter
         {
+            /// <summary>
+            /// Returns a copy of an image with values ranging from 0 to T.MaxValue
+            /// Assumes input image values are normalized 0-1 for most types
+            /// No scaling is performed on float or double types
+            /// </summary>
+            /// <typeparam name="T">Type used to deetermine the output value range</typeparam>
+            /// <param name="image"></param>
+            /// <returns></returns>
             public Image Convert<T>(Image image)
             {
                 Image converted = (Image)image.Clone();
@@ -95,5 +99,19 @@ namespace OPS.Imaging
             }
         }
 
+        private class ValuePassThrough : IImageConverter
+        {
+            /// <summary>
+            /// Simply copies the image and returns it without modifying the values
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <param name="image"></param>
+            /// <returns></returns>
+            public Image Convert<T>(Image image)
+            {
+                Image converted = (Image)image.Clone();
+                return converted;
+            }
+        }
     }
 }
