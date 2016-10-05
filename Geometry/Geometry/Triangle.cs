a6ef7b2d533569060aaf5fa0ab00cb2b4f9ad7b9
menzies <menzies@jpl.nasa.gov>
2016-10-04 18:26:29 -0700

    Add basic structure for mesh class

---
+++ Geometry/Geometry/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OPS.Geometry
+{
+    public class Triangle
+    {
+        public Vertex V0;
+        public Vertex V1;
+        public Vertex V2;
+
+        public Triangle()
+        {
+
+        }
+
+        public Triangle(Vertex v0, Vertex v1, Vertex v2)
+        {
+            this.V0 = v0;
+            this.V1 = v1;
+            this.V2 = v2;
+        }
+    }
+}
---
+++ Geometry/Geometry/Vertex.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace OPS.Geometry
+{
+    public class Vertex
+    {
+        public Vector3 Position;
+        public Vector3 Normal;
+        public Vector3 Color;
+        public Vector2 UV;
+
+        public override bool Equals(System.Object obj)
+        {
+            return Equals(obj as Vertex);
+        }
+
+        public bool Equals(Vertex v)
+        {
+            // For Equals implementation see https://msdn.microsoft.com/en-us/library/dd183755.aspx 
+            // If parameter is null, return false.
+            if (Object.ReferenceEquals(v, null))
+            {
+                return false;
+            }
+
+            // Optimization for a common success case.
+            if (Object.ReferenceEquals(this, v))
+            {
+                return true;
+            }
+
+            // If run-time types are not exactly the same, return false.
+            if (this.GetType() != v.GetType())
+            {
+                return false;
+            }
+
+            // Return true if the fields match.
+            // Note that the base class is not invoked because it is
+            // System.Object, which defines Equals as reference equality.
+            return (Position == v.Position) && (Normal == v.Normal) && (Color == v.Color) && (UV == v.UV);
+        }
+
+
+        public override int GetHashCode()
+        {
+            return  ((int)Position.X) ^ ((int)Position.Y*10) ^ ((int)Position.Z*100) ^ ((int)UV.X * 100) ^ ((int)UV.Y * 100);
+        }
+    }
+}
