b32a1a3f784bf6f35b7b9d85415db3dabec7ed02
menzies <menzies@jpl.nasa.gov>
2017-01-24 17:26:19 -0800

    Refactor Vertex structure so that values are not nullable.  Make having nomrals, uvs, or color a property of the mesh class

--- Geometry/Geometry/Mesh.cs
+++ Geometry/Geometry/Mesh.cs
@@ -5,21 +5,57 @@ using System.Text;
 
 namespace OPS.Geometry
 {
+    /// <summary>
+    /// A class representing a 3D mesh
+    /// 
+    /// Each mesh is comprised of a list of vertices and a faces
+    /// 
+    /// Each of the vertices must have a valid position value, but all other properties of the vertex structure
+    /// are optional.  Properties are defined on a per mesh basis, either a property is defined for all of 
+    /// a mesh's vertices or none of them.  The flags controlled by SetProperties determine what properties
+    /// a mesh has and undefined properties are ingored by meshing operations.
+    /// 
+    /// </summary>
     public class Mesh
     {
         public List<Face> Faces;
         public List<Vertex> Vertices;
-       
-        public Mesh(int capacity=10)
+        
+        public bool HasNormals = false;
+        public bool HasUVs = false;
+        public bool HasColors = false;
+
+        /// <summary>
+        /// Creates an empty mesh. 
+        /// </summary>
+        /// <param name="capacity"></param>
+        public Mesh(int capacity=10, bool hasNormals = false, bool hasUVs = false, bool hasColors = false)
         {
             Faces = new List<Face>(capacity);
             Vertices = new List<Vertex>(capacity);
+            SetProperties(hasNormals, hasUVs, HasColors);
         }
 
-        public Mesh(List<Triangle> triangles)
+        public Mesh(Mesh other)
+        {
+            Faces = new List<Face>(other.Faces.Count);
+            for(int i = 0; i < Faces.Count; i++)
+            {
+                Faces[i] = other.Faces[i];
+            }
+            Vertices = new List<Vertex>(other.Vertices.Count);
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                Vertices[i] = new Vertex(other.Vertices[i]);
+            }
+            SetProperties(other.HasNormals, other.HasUVs, other.HasColors);
+        }
+
+        public Mesh(List<Triangle> triangles, bool hasNormals = false, bool hasUVs = false, bool hasColors = false)
         {
             Faces = new List<Face>(triangles.Count);
             Vertices = new List<Vertex>(triangles.Count * 3);
+            SetProperties(hasNormals, hasUVs, HasColors);
             int idx = 0;
             foreach (Triangle t in triangles)
             {
@@ -29,6 +65,13 @@ namespace OPS.Geometry
                 Vertices.Add(t.V1);
                 Vertices.Add(t.V2);
             }
+        }
+
+        public void SetProperties(bool hasNormals, bool hasUVs, bool hasColors)
+        {
+            this.HasNormals = hasNormals;
+            this.HasUVs = hasUVs;
+            this.HasColors = hasColors;
         }
 
         public List<Triangle> Triangles()
--- Geometry/Geometry/Vertex.cs
+++ Geometry/Geometry/Vertex.cs
@@ -8,10 +8,35 @@ namespace OPS.Geometry
 {
     public class Vertex
     {
+        /// <summary>
+        /// Required property of all Vetex objects
+        /// </summary>
         public Vector3 Position;
-        public Vector3? Normal;
-        public Vector3? Color;
-        public Vector2? UV;
+        /// <summary>
+        /// Optional property
+        /// </summary>
+        public Vector3 Normal;
+        /// <summary>
+        /// Optional property
+        /// </summary>
+        public Vector3 Color;
+        /// <summary>
+        /// Optional property
+        /// </summary>
+        public Vector2 UV;
+
+        public Vertex()
+        {
+
+        }
+
+        public Vertex(Vertex other)
+        {
+            this.Position = other.Position;
+            this.Normal = other.Normal;
+            this.Color = other.Color;
+            this.UV = other.UV;
+        }
 
         public override bool Equals(System.Object obj)
         {
@@ -42,17 +67,13 @@ namespace OPS.Geometry
             // Return true if the fields match.
             // Note that the base class is not invoked because it is
             // System.Object, which defines Equals as reference equality.
-            return (Position == v.Position) && (Normal.Value == v.Normal.Value) && (Color.Value == v.Color.Value) && (UV.Value == v.UV.Value);
+            return (Position == v.Position) && (Normal == v.Normal) && (Color == v.Color) && (UV == v.UV);
         }
 
 
         public override int GetHashCode()
         {
-            int uvValue = 0;
-            if(UV.HasValue)
-            {
-                uvValue = ((int)UV.Value.X * 100) ^ ((int)UV.Value.Y * 100);
-            }
+            int uvValue = uvValue = ((int)UV.X * 100) ^ ((int)UV.Y * 100);
             return  ((int)Position.X) ^ ((int)Position.Y*10) ^ ((int)Position.Z*100) ^ uvValue;
         }
     }
