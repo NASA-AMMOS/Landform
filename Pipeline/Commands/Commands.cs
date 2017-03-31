5bdc184dc94ad7ccb381165245f79955746a58dc
menzies <menzies@jpl.nasa.gov>
2017-03-30 21:23:46 -0700

    Added command to convert baseline open inventor mesh to different formats

--- Pipeline/Commands/Commands.cs
+++ Pipeline/Commands/Commands.cs
@@ -10,15 +10,18 @@ namespace OPS.Pipeline
     public class Commands
     {
         /// <summary>
-        /// Parses command line arguments and executes the appropriate command
+        /// Parses command line arguments and executes the appropriate command        
         /// </summary>
         /// <param name="args"></param>
         /// <returns></returns>
         public static int RunFromCommandline(string[] args)
         {
-            return CommandLine.Parser.Default.ParseArguments<CralwMSLOptions>(args)
+            /// Commands are defined by the list of types passed into ParseArguments
+            /// Each passed in object must have a [Verb] decorator
+            return CommandLine.Parser.Default.ParseArguments<CralwMSLOptions, ConvertBaselineMeshOptions>(args)
               .MapResult(
                 (CralwMSLOptions opts) => new CrawlMSL(opts).Run(),
+                (ConvertBaselineMeshOptions opts) => new ConvertBaselineMesh(opts).Run(),
                 errs => 1);
         }
     }
