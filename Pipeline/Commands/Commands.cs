ca9cd8fe429697fa46e352e23aa80ce833ff46cb
menzies <menzies@jpl.nasa.gov>
2017-04-12 18:14:02 -0700

    Added a command to perform test s3 read performance

--- Pipeline/Commands/Commands.cs
+++ Pipeline/Commands/Commands.cs
@@ -18,10 +18,11 @@ namespace OPS.Pipeline
         {
             /// Commands are defined by the list of types passed into ParseArguments
             /// Each passed in object must have a [Verb] decorator
-            return CommandLine.Parser.Default.ParseArguments<CralwMSLOptions, ConvertBaselineMeshOptions>(args)
+            return CommandLine.Parser.Default.ParseArguments<CralwMSLOptions, ConvertBaselineMeshOptions, BenchmarkS3Options>(args)
               .MapResult(
                 (CralwMSLOptions opts) => new CrawlMSL(opts).Run(),
                 (ConvertBaselineMeshOptions opts) => new ConvertBaselineMesh(opts).Run(),
+                (BenchmarkS3Options opts) => new BenchmarkS3(opts).Run(),
                 errs => 1);
         }
     }
