1d69959b680ea92734bc29b7868fb9443018b6ed
menzies <menzies@jpl.nasa.gov>
2017-06-02 11:48:19 -0700

    Created command for image match experimentation.

--- Pipeline/Commands/Commands.cs
+++ Pipeline/Commands/Commands.cs
@@ -18,11 +18,12 @@ namespace OPS.Pipeline
         {
             /// Commands are defined by the list of types passed into ParseArguments
             /// Each passed in object must have a [Verb] decorator
-            return CommandLine.Parser.Default.ParseArguments<CralwMSLOptions, ConvertBaselineMeshOptions, BenchmarkS3Options>(args)
+            return CommandLine.Parser.Default.ParseArguments<CralwMSLOptions, ConvertBaselineMeshOptions, BenchmarkS3Options, MatchImagesOptions>(args)
               .MapResult(
                 (CralwMSLOptions opts) => new CrawlMSL(opts).Run(),
                 (ConvertBaselineMeshOptions opts) => new ConvertBaselineMesh(opts).Run(),
                 (BenchmarkS3Options opts) => new BenchmarkS3(opts).Run(),
+                (MatchImagesOptions opts) => new MatchImages(opts).Run(),
                 errs => 1);
         }
     }
---
+++ Pipeline/Commands/MatchImages.cs
@@ -0,0 +1,35 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPS.Pipeline
+{
+
+    [Verb("matchimages", HelpText = "")]
+    public class MatchImagesOptions
+    {
+        [Value(0, Required = true, HelpText = "")]
+        public string ImageA { get; set; }
+
+        [Value(1, Required = true, HelpText = "")]
+        public string ImageB { get; set; }
+
+    }
+
+    public class MatchImages
+    {
+        public MatchImagesOptions options;
+        public MatchImages(MatchImagesOptions options)
+        {
+            this.options = options;
+        }
+
+        public int Run()
+        {
+            return 0;
+        }
+    }
+}
