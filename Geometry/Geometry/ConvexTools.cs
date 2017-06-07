b7581648418ca1b4291ff898852c21e5a7d6b29e
Charles Goddard <goddard@jpl.nasa.gov>
2017-06-07 15:46:53 -0700

    Add convex hull stuff

---
+++ Geometry/Geometry/ConvexTools.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MIConvexHull;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+
+namespace OPS.Geometry
+{
+    public static class ConvexTools
+    {
+        public static Mesh ConvexHull(this Mesh self)
+        {
+            List<double[]> pointArray = new List<double[]>();
+            for (int i = 0; i < self.Vertices.Count; i++)
+            {
+                var vert = self.Vertices[i];
+                pointArray.Add(new double[] { vert.Position.X, vert.Position.Y, vert.Position.Z });
+            }
+            var hull = MIConvexHull.ConvexHull.Create(pointArray);
+            var faces = hull.Faces.ToArray();
+            var res = new Mesh(true, false, false, faces.Length * 3);
+            for (int i = 0; i < faces.Length; i++)
+            {
+                var f = faces[i];
+                int baseIdx = res.Vertices.Count;
+                for (int j = 0; j < f.Vertices.Length; j++)
+                {
+                    int idx = res.Vertices.Count;
+                    res.Vertices.Add(new Vertex(
+                        new Vector3(f.Vertices[j].Position),
+                        Vector3.Normalize(new Vector3(f.Normal)),
+                        Vector4.Zero,
+                        Vector2.Zero
+                        ));
+                }
+                res.Faces.Add(new Face(Enumerable.Range(baseIdx, f.Vertices.Length).ToArray()));
+            }
+            return res;
+        }
+
+        public static bool PointInside(Mesh hull, Vector3 point)
+        {
+            if (!hull.HasNormals) throw new ArgumentException("must have vertex normals equal to face normals");
+            for (int i = 0; i < hull.Faces.Count; i++)
+            {
+                var f = hull.Faces[i];
+                var v = hull.Vertices[f.P0];
+
+                var norm = v.Normal;
+#if DEBUG
+                // sanity check vertex normals - must be equal to face normal
+                {
+                    var v0 = hull.Vertices[f.P0].Position;
+                    var v1 = hull.Vertices[f.P1].Position;
+                    var v2 = hull.Vertices[f.P2].Position;
+                    Vector3 faceNorm = (v1 - v0).Cross(v2 - v0);
+                    faceNorm.Normalize();
+                    Debug.Assert(faceNorm.AlmostEqual(norm));
+                }
+#endif
+
+                double dist = v.Position.Dot(norm);
+                if (point.Dot(norm) > dist)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool Intersects(Mesh c0, Mesh c1)
+        {
+            return GJKIntersection.Intersects(c0, c1);
+        }
+    }
+}
---
+++ Geometry/Geometry/GJKIntersection.cs
@@ -0,0 +1,267 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPS.Geometry
+{
+    // from http://programyourfaceoff.blogspot.com/2012/01/gjk-algorithm.html
+    public static class GJKIntersection
+    {
+        static Vector3 FurthestPoint(Mesh m, Vector3 dir)
+        {
+            double maxDot = double.NegativeInfinity;
+            Vector3 res = dir * double.NegativeInfinity;
+            foreach (var vtx in m.Vertices)
+            {
+                var v = vtx.Position;
+                if (v.Dot(dir) > maxDot)
+                {
+                    maxDot = v.Dot(dir);
+                    res = v;
+                }
+            }
+            return res;
+        }
+        static Vector3 Support(Mesh one, Mesh two, Vector3 dir)
+        {
+            return FurthestPoint(one, dir) - FurthestPoint(two, -dir);
+        }
+
+        public static bool Intersects(Mesh one, Mesh two)
+        {
+            Vector3 d = Vector3.One / Math.Sqrt(3);
+            Vector3 s = Support(one, two, d);
+            List<Vector3> simplex = new List<Vector3>() { s };
+            d = -s;
+
+            int maxIters = 100;
+            for (int i = 0; i < maxIters; i++)
+            {
+                d.Normalize();
+                Vector3 a = Support(one, two, d);
+                if (a.Dot(d) < 0) return false;
+
+                simplex.Add(a);
+                if (ProcessSimplex(ref simplex, ref d)) return true;
+            }
+            return true; // eh, probably intersects?
+        }
+
+        private static bool ProcessSimplex(ref List<Vector3> simplex, ref Vector3 d)
+        {
+            if (simplex.Count == 1)
+            {
+                d = -simplex[0];
+            }
+            if (simplex.Count == 2) return ProcessLine(ref simplex, ref d);
+            else if (simplex.Count == 3) return ProcessTriangle(ref simplex, ref d);
+            else return ProcessTetrahedron(ref simplex, ref d);
+        }
+
+        private static bool ProcessLine(ref List<Vector3> simplex, ref Vector3 d)
+        {
+            Vector3 a = simplex[1];
+            Vector3 b = simplex[0];
+            Vector3 ab = b - a;
+            Vector3 a0 = -a;
+            if (ab.Dot(a0) > 0)
+            {
+                d = Vector3.Cross(Vector3.Cross(ab, a0), ab);
+                if (d.LengthSquared() < 1e-7)
+                {
+                    // a0 and ab colinear
+                    d = new Vector3(-d.X, 0, 0);
+                }
+            }
+            else
+            {
+                simplex.Remove(b);
+                d = a0;
+            }
+            return false;
+        }
+
+        private static bool ProcessTriangle(ref List<Vector3> simplex, ref Vector3 d)
+        {
+            Vector3 a = simplex[2];
+            Vector3 b = simplex[1];
+            Vector3 c = simplex[0];
+            Vector3 ab = b - a;
+            Vector3 ac = c - a;
+            Vector3 abc = Vector3.Cross(ab, ac);
+            Vector3 a0 = -a;
+            Vector3 acNormal = Vector3.Cross(abc, ac);
+            Vector3 abNormal = Vector3.Cross(ab, abc);
+
+            if (acNormal.Dot(a0) > 0)
+            {
+                if (ac.Dot(a0) > 0)
+                {
+                    simplex.Remove(b);
+                    d = Vector3.Cross(Vector3.Cross(ac, a0), ac);
+                }
+                else
+                {
+                    if (ab.Dot(a0) > 0)
+                    {
+                        simplex.Remove(c);
+                        d = Vector3.Cross(Vector3.Cross(ab, a0), ab);
+                    }
+                    else
+                    {
+                        simplex.Remove(b);
+                        simplex.Remove(c);
+                        d = a0;
+                    }
+                }
+            }
+            else
+            {
+                if (abNormal.Dot(a0) > 0)
+                {
+                    if (ab.Dot(a0) > 0)
+                    {
+                        simplex.Remove(c);
+                        d = Vector3.Cross(Vector3.Cross(ab, a0), ab);
+                    }
+                    else
+                    {
+                        simplex.Remove(b);
+                        simplex.Remove(c);
+                        d = a0;
+                    }
+                }
+                else
+                {
+                    if (abc.Dot(a0) > 0)
+                    {
+                        d = Vector3.Cross(Vector3.Cross(abc, a0), abc);
+                    }
+                    else
+                    {
+                        d = Vector3.Cross(Vector3.Cross(-abc, a0), -abc);
+                    }
+                }
+            }
+            return false;
+
+            /*if (abNormal.Dot(a0) > 0)
+            {
+                simplex = new List<Vector3>() { a, b };
+                d = ab.Cross(a0).Cross(ab);
+                return false;
+            }
+
+            if (acNormal.Dot(a0) > 0)
+            {
+                simplex = new List<Vector3>() { a, c };
+                d = ac.Cross(a0).Cross(ac);
+                return false;
+            }
+
+            if (abc.Dot(a0) > 0)
+            {
+                d = abc;
+            }
+            else
+            {
+                simplex = new List<Vector3>() { a, c, b };
+                d = -abc;
+            }
+            return false;*/
+        }
+
+        /*private static bool _innerTetrahedron(ref List<Vector3> simplex, Vector3 a, Vector3 b, Vector3 c, Vector3 d, ref Vector3 direction)
+        {
+            Vector3 abNormal = null; 
+        }*/
+        private static bool ProcessTetrahedron(ref List<Vector3> simplex, ref Vector3 direction)
+        {
+            Vector3 a = simplex[3];
+            Vector3 b = simplex[2];
+            Vector3 c = simplex[1];
+            Vector3 d = simplex[0];
+            Vector3 ac = c - a;
+            Vector3 ad = d - a;
+            Vector3 ab = b - a;
+            Vector3 bc = c - b;
+            Vector3 bd = d - b;
+
+            Vector3 acd = Vector3.Cross(ad, ac);
+            Vector3 abd = Vector3.Cross(ab, ad);
+            Vector3 abc = Vector3.Cross(ac, ab);
+
+            Vector3 a0 = -a;
+
+            if (abc.Dot(a0) > 0)
+            {
+                if (Vector3.Cross(abc, ac).Dot(a0) > 0)
+                {
+                    simplex.Remove(b);
+                    simplex.Remove(d);
+                    direction = Vector3.Cross(Vector3.Cross(ac, a0), ac);
+                }
+                else if (Vector3.Cross(ab, abc).Dot(a0) > 0)
+                {
+                    simplex.Remove(c);
+                    simplex.Remove(d);
+                    direction = Vector3.Cross(Vector3.Cross(ab, a0), ab);
+                }
+                else
+                {
+                    simplex.Remove(d);
+                    direction = abc;
+                }
+            }
+            else if (acd.Dot(a0) > 0)
+            {
+                if (Vector3.Cross(acd, ad).Dot(a0) > 0)
+                {
+                    simplex.Remove(b);
+                    simplex.Remove(c);
+                    direction = Vector3.Cross(Vector3.Cross(ad, a0), ad);
+                }
+                else if (Vector3.Cross(ac, acd).Dot(a0) > 0)
+                {
+                    simplex.Remove(b);
+                    simplex.Remove(d);
+                    direction = Vector3.Cross(Vector3.Cross(ac, a0), ac);
+                }
+                else
+                {
+                    simplex.Remove(b);
+                    direction = acd;
+                }
+            }
+            else if (abd.Dot(a0) > 0)
+            {
+                if (Vector3.Cross(abd, ab).Dot(a0) > 0)
+                {
+                    simplex.Remove(c);
+                    simplex.Remove(d);
+                    direction = Vector3.Cross(Vector3.Cross(ab, a0), ab);
+                }
+                else if (Vector3.Cross(ad, abd).Dot(a0) > 0)
+                {
+                    simplex.Remove(b);
+                    simplex.Remove(c);
+                    direction = Vector3.Cross(Vector3.Cross(ad, a0), ad);
+                }
+                else
+                {
+                    simplex.Remove(c);
+                    direction = abd;
+                }
+            }
+            else
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
