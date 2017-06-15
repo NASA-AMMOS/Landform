4e36fd73042912ea512259112b1312031c90dd48
menzies <menzies@jpl.nasa.gov>
2017-06-15 14:40:37 -0700

    Added SceneContent and SceneNode that will soon be replaced by versions in pipeline library

---
+++ Geometry/Geometry/SceneContent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPS.Geometry
+{
+    // TODO: replace with upcoming Pipeline/Scene/NodeComponent
+    public class SceneContent
+    {
+    }
+}
