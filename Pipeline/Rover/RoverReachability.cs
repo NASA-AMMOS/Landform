df9d737b34e2da297aa00efade731836d6302029
menzies <menzies@jpl.nasa.gov>
2017-06-29 16:22:03 -0700

    Improved baseline mesh converter and added convertbaselinemeshes command for converting multiple meshes

---
+++ Pipeline/Rover/RoverReachability.cs
@@ -0,0 +1,48 @@
+﻿using OPS.Imaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPS.Pipeline
+{
+    public class RoverReachability
+    {
+        /// <summary>
+        /// Given an image and a corresponding reachability product, create a new image where alpha is determined by 
+        /// reachability.  Alpha will be 1 in areas that are reachable, other areas it will be set to the supplied value.
+        /// A pixel is deemed reachable if it is reachable in any channel of the input product.
+        /// </summary>
+        /// <param name="img"></param>
+        /// <param name="reachability"></param>
+        /// <param name="unreachableAlpha"></param>
+        /// <returns></returns>
+        public static Image GenerateAlphaFromArmImage(Image img, Image reachability, float unreachableAlpha = 0.75f)
+        {
+            Image r = new Image(img);
+            float[][] origData = r.Data;
+            r.Data = new float[img.Bands + 1][];
+            r.Data[img.Bands] = new float[img.Width * img.Height];
+            r.Bands += 1;
+
+            for (int b = 0; b < img.Bands; b++)
+            {
+                r.Data[b] = origData[b];
+            }
+            for (int i = 0; i < r.Data[0].Length; i++)
+            {
+                r.Data[r.Bands - 1][i] = unreachableAlpha;
+                for (int b = 0; b < reachability.Bands; b++)
+                {
+                    if (reachability.Data[b][i] != 0)
+                    {
+                        r.Data[r.Bands - 1][i] = 1;
+                        break;
+                    }
+                }
+            }
+            return r;
+        }
+    }
+}
---
+++ PipelineTest/Rover/RoverCoordinateSystemTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+using OPS.Pipeline;
+using OPS.Geometry;
+
+namespace PipelineTest
+{
+    [TestClass]
+    public class RoverCoordinateSystemTest
+    {
+        [TestMethod]
+        public void TestLocalLevelUnityConversion()
+        {
+            Vector3 localLevel = new Vector3(1, 2, 3);
+            Vector3 unity = Vector3.Transform(localLevel, RoverCoordinateSystem.LocalLevelToUnity);
+            Assert.AreEqual(new Vector3(2,-3,1), unity);
+            Assert.AreEqual(localLevel, Vector3.Transform(unity, RoverCoordinateSystem.UnityToLocalLevel));
+
+            Mesh m = new Mesh();
+            m.Vertices.Add(new Vertex(localLevel));
+            RoverCoordinateSystem.LocalLevelToUnityMesh(m);
+            Assert.AreEqual(unity, m.Vertices[0].Position);
+            RoverCoordinateSystem.UnityToLocalLevelMesh(m);
+            Assert.AreEqual(localLevel, m.Vertices[0].Position);
+        }
+    }
+}
