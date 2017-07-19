5c50901273058546e9b345d718542199e17263c1
menzies <menzies@jpl.nasa.gov>
2017-07-19 14:03:36 -0700

    Added locations.xml parser for MSL

---
+++ Pipeline/Rover/SiteDrive.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPS.Pipeline
+{
+    public struct SiteDrive
+    {
+        public int Site;
+        public int Drive;
+
+        public SiteDrive(int site, int drive)
+        {
+            this.Site = site;
+            this.Drive = drive;
+        }
+
+        public SiteDrive(string name)
+        {
+            if (name.Length != 10)
+            {
+                throw new ArgumentException("Unexpected sitedrive string length");
+            }
+            this.Site = int.Parse(name.Substring(0, 5));
+            this.Drive = int.Parse(name.Substring(5, 5));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0:D5}{1:D5}", Site, Drive);
+            
+        }
+
+        public override int GetHashCode()
+        {
+            return Site * 10000 + Drive;
+        }
+    }
+}
