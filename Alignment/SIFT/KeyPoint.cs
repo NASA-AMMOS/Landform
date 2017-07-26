56c9390b92fe187b064359ce1086d0be7b4956d3
JPL\charchut <nicholas.g.charchut@jpl.nasa.gov>
2017-07-26 14:44:40 -0700

    began porting of SIFT to CS

---
+++ Alignment/SIFT/KeyPoint.cs
@@ -0,0 +1,13 @@
+﻿namespace OPS.Pipeline
+{
+    internal class KeyPoint
+    {
+        public int octave, layer, x, y;
+        public float size, orientation;
+
+        public KeyPoint()
+        {
+
+        }
+    }
+}
