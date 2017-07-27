3f94522662e472f3876c38dc3c081ab8f3518968
menzies <menzies@jpl.nasa.gov>
2017-07-26 19:25:32 -0700

    Updated command line build script.  Fixed test errors caused by missing gdal native package in ImagaingEmguTest, and missing coin3.dll in Open Inventor converter.  Also updated package reference to Emgu to 4.6.2

--- build.bat
+++ build.bat
@@ -1,13 +1,12 @@
-msbuild /t:Clean 
-msbuild /p:Configuration=Release
+
+rmdir /S /Q out
+msbuild /t:Clean,Build /p:Configuration=Release
 if %errorlevel% neq 0 exit /b %errorlevel%
 runTests.bat
 if %errorlevel% neq 0 exit /b %errorlevel%
-cd Pipeline
-nuget pack Pipeline.csproj -IncludeReferencedProjects -properties Configuration=Release
-cd ../
-rmdir /S /Q out
+nuget pack Pipeline\Pipeline.csproj -IncludeReferencedProjects -properties Configuration=Release
+if %errorlevel% neq 0 exit /b %errorlevel%
 mkdir out
-move Pipeline\JPL.Landform*nupkg out
+move JPL.Landform*nupkg out
 mkdir out\Landform
 copy Landform\bin\Release\* out\Landform
