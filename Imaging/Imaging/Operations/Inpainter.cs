d875f9a8cae33b54e685f6db0ab2e9214bc6490d
menzies <menzies@jpl.nasa.gov>
2017-08-08 18:45:17 -0700

    Small cleanup to inpainter

--- Imaging/Imaging/Operations/Inpainter.cs
+++ Imaging/Imaging/Operations/Inpainter.cs
@@ -22,13 +22,21 @@ namespace OPS.Imaging
         static bool HasNeighbors(int r, int c, Image image)
         {
             if (r > 0 && image.IsValid(r - 1, c))
+            {
                 return true;
+            }
             if (c > 0 && image.IsValid(r, c - 1))
+            {
                 return true;
+            }
             if (r < image.Height - 1 && image.IsValid(r + 1, c))
+            {
                 return true;
+            }
             if (c < image.Width - 1 && image.IsValid(r, c + 1))
+            {
                 return true;
+            }
             return false;
         }
 
@@ -38,13 +46,10 @@ namespace OPS.Imaging
         /// <param name="r"></param>
         /// <param name="c"></param>
         /// <param name="image"></param>
-        static void Pad(int r, int c, Image image)
+        static void FillWithNeighborAverage(int r, int c, Image image)
         {
             float num = 0;
             float[] average = new float[image.Bands];
-            for (int b = 0; b < image.Bands; b++)
-                average[b] = 0;
-
             for (int r2 = Math.Max(0, r - 1); r2 <= Math.Min(image.Height - 1, r + 1); r2++)
             {
                 for (int d2 = Math.Max(0, c - 1); d2 <= Math.Min(image.Width - 1, c + 1); d2++)
@@ -52,17 +57,27 @@ namespace OPS.Imaging
                     if (image.IsValid(r2, d2))
                     {
                         num++;
+                        var bandValues = image.GetBandValues(r2, d2);
                         for (int b = 0; b < image.Bands; b++)
-                            average[b] += image.GetBandValues(r2, d2)[b];
+                        {
+                            average[b] += bandValues[b];
+                        }
                     }
                 }
             }
-
+            // This method should only be called when at least one neighbor is valid so num should never be zero
             for (int b = 0; b < image.Bands; b++)
+            {
                 average[b] /= num;
+            }
             image.SetBandValues(r, c, average);
         }
 
+        /// <summary>
+        /// Inpaint masked reagions of an image by the amount specified by pad width
+        /// </summary>
+        /// <param name="image"></param>
+        /// <param name="padWidth"></param>
         public static void Apply(Image image, int padWidth = -1)
         {
             if(!image.HasMask)
@@ -71,11 +86,8 @@ namespace OPS.Imaging
             }
             // in paint set up:
             //   add "edge points" to the mask of image, and store them in a new list
-            //Image imageCopy = (Image)image.Clone();
-
             List<Vector2> edgePoints = new List<Vector2>();
             List<Vector2> newEdgePoints = new List<Vector2>();
-
             for (int r = 0; r < image.Height; r++)
             {
                 for (int c = 0; c < image.Width; c++)
@@ -85,22 +97,23 @@ namespace OPS.Imaging
                         edgePoints.Add(new Vector2(r, c));
                     }
                 }
-            }
-      
+            }      
             // in paint:
             //   Populate current edge points in image
             //   Use edge points to get new list of edge points and continue padding outwards
             for (int i = 0; i != padWidth; i++)
             {
                 if (edgePoints.Count == 0)
+                {
                     break;
+                }
                 foreach (Vector2 edge in edgePoints)
                 {
                     image.SetMaskValue((int)edge.X, (int)edge.Y, true);
                 }
                 foreach (Vector2 edge in edgePoints)
                 {
-                    Pad((int)edge.X, (int)edge.Y, image);
+                    FillWithNeighborAverage((int)edge.X, (int)edge.Y, image);
                 }   
                 foreach (Vector2 edge in edgePoints)
                 {
