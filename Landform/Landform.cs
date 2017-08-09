e5426c24d265877eec53d6987f595fa8c33c0670
Keavon Chambers <keavon.e.chambers@jpl.nasa.gov>
2017-08-09 14:30:00 -0700

    Change Landform.cs file back to original

--- Landform/Landform.cs
+++ Landform/Landform.cs
@@ -9,14 +9,14 @@ using OPS.Geometry;
 using OPS.Cloud;
 using OPS.Pipeline;
 using log4net;
-using Microsoft.Xna.Framework;
+using OPS.Util;
 
 namespace Landform
 {
     class Landform
     {
         static ILog logger = LogManager.GetLogger(typeof(Landform));
-
+        
         /// <summary>
         /// The start of everything
         /// </summary>
@@ -24,26 +24,15 @@ namespace Landform
         /// <returns></returns>
         static int Main(string[] args)
         {
+            Config.ApplicationConfigFolder = ".landform";
             // Enable logging
             log4net.Config.XmlConfigurator.Configure();
             // Register filetype handlers
             new OpenInventorSerializer().Register();
-            // Load mesh
-            Mesh mesh = Mesh.Load(@"C:\Users\kchamber.JPL\Desktop\colors.ply");
 
-            // Sample points on mesh
-            Mesh pointCloud = SurfacePointSample.GenerateSampledMesh(mesh, 300);
-
-            // return 0;
-
-            // Save and open mesh
-            // Mesh r = MeshLab.Sample(mesh, 20000);
-            pointCloud.Save(@"C:\Users\kchamber.JPL\Desktop\points.obj");
-            System.Diagnostics.Process.Start(@"C:\Users\kchamber.JPL\Desktop\points.obj");
-            return 0;
             // Parse command line arguments
-            // int returnCode = Commands.RunFromCommandline(args);
-            // return returnCode;
+            int returnCode = Commands.RunFromCommandline(args);
+            return returnCode;
         }
     }
 }
