9da37c0015202a4793572a7dd3b7e4ec65785db2
Keavon Chambers <keavon.e.chambers@jpl.nasa.gov>
2017-09-08 20:39:28 -0700

    Hopefully these are enough comments?

--- Geometry/Geometry/HausdorffDistance.cs
+++ Geometry/Geometry/HausdorffDistance.cs
@@ -10,12 +10,31 @@ using System.Diagnostics;
 
 namespace OPS.Geometry
 {
+    /// <summary>
+    /// Computes the distance between the surfaces of two meshes
+    /// Based on "Fast and Accurate Hausdorff Distance Calculation Between Meshes"
+    /// By Michael Guthe, Pavel Borodin, and Reinhard Klein
+    /// http://cg.cs.uni-bonn.de/en/publications/paper-details/guthe-2005-fast/
+    /// </summary>
     public class HausdorffDistance
     {
+        // The square of the Hausdorff distance, which starts at 0 and grows until the algorithm ends, when it returns its square root
         private double hausdorffDistanceSquared;
+
+        // Priority queue holding the octree node cells and also triangles themselves which get directed by the cell or tree's maximum possible distance to another triangle
         private SimplePriorityQueue<OctreeNode> queue;
+
+        // The max error in distance allowed when the algorithm runs, telling it when it can stop subdividing for higher accuracy
+        // Subdivision is disabled when 0, meaning it runs fast but isn't accurate
         private double epsilon;
 
+        /// <summary>
+        /// Calculates the bidirectional Hausdorff error distance (in model units) between mesh A and B within an allowed error distance (in model units)
+        /// </summary>
+        /// <param name="meshA">The first model</param>
+        /// <param name="meshB">The second model</param>
+        /// <param name="maxErrorEpsilon">Maximum allowed uncertainty in the answer, where accuracy comes at the cost of speed</param>
+        /// <returns></returns>
         public double Calculate(Mesh meshA, Mesh meshB, double maxErrorEpsilon)
         {
             // Start with zero minimum distance and an empty priority queue
@@ -26,12 +45,15 @@ namespace OPS.Geometry
             // Build an octree for both meshes and insert the mesh triangles into them
             BoundingBox combinedBounds = BoundingBoxExtensions.Union(new List<BoundingBox> { meshA.Bounds(), meshB.Bounds() });
 
-            Octree a = new Octree(combinedBounds);
-            Octree b = new Octree(combinedBounds);
+            // Build two separate octrees with the same bounds so that each cell can correspond to a cell in the other octree
+            Octree octreeA = new Octree(combinedBounds);
+            Octree octreeB = new Octree(combinedBounds);
 
+            // List of triangles from each respective mesh, with each triangle held in a wrapper structure that fits in the octree
             List<OctreeNodeContents> triListA = new List<OctreeNodeContents>();
             List<OctreeNodeContents> triListB = new List<OctreeNodeContents>();
 
+            // Fill each list of triangles with all the triangles from their respective mesh
             foreach (Triangle tri in meshA.Triangles())
             {
                 triListA.Add(new VoxelTriangle(tri));
@@ -41,43 +63,59 @@ namespace OPS.Geometry
                 triListB.Add(new VoxelTriangle(tri));
             }
 
-            a.InsertList(triListA);
-            b.InsertList(triListB);
+            // Insert all the triangles from each mesh into their respective octree
+            octreeA.InsertList(triListA);
+            octreeB.InsertList(triListB);
 
-            // Cache the traversal path in each voxel triangle
+            // Action function used to cache the traversal path in each voxel triangle to reach its containing octree node cell
             Action<OctreeNode> saveTraversal = (node => {
+                // List of traversal steps (each from 0-7) used to go down through the branches of the tree
                 List<int> path = new List<int>();
 
+                // Walk up the tree, updating the current to the new node and adding the branch index to the traversal path list
                 OctreeNode current = node;
                 while (current.Parent != null)
                 {
+                    // Check each branch in the parent to see if it's equal to the current branch we are walking on
                     int i = 0;
                     foreach (OctreeNode child in current.Parent.Children)
                     {
+                        // If this is the same branch as myself, use stop checking all the other branches after me
                         if (child == current)
                         {
                             break;
                         }
+                        
+                        // This current node must be in a later branch owned by the parent of this node
                         i++;
                     }
 
+                    // Once it breaks or finishes the loop and has the correct value of i, that's the index of the current node
                     path.Add(i);
+
+                    // Update the current node to its parent so it walks one level up the tree, where it can eventually stop looping upon reaching null (the root)
                     current = current.Parent;
                 }
 
+                // Place the traversal path in the triangle structure so it knows how to reach its octree node cell
                 foreach (VoxelTriangle voxelTri in node.Contained)
                 {
+                    // Traversal goes from the bottom up, so it must be reversed to traverse from the top back down
                     List<int> reversedPath = new List<int>(path);
                     reversedPath.Reverse();
+
+                    // Put the correctly ordered traversal path on the voxel triangle
                     voxelTri.TraversalPath = reversedPath;
                 }
             });
-            a.Traverse(saveTraversal);
-            b.Traverse(saveTraversal);
+
+            // Traverse all the nodes in both octrees and determine then cache the traversal path for every voxel triangle in the tree
+            octreeA.Traverse(saveTraversal);
+            octreeB.Traverse(saveTraversal);
 
             // Enqueue the root cell of both octrees
-            queue.Enqueue(a.Root, (float)ComputeMaxGeometricDistanceToClosestCellSquared(a.Root, b));
-            queue.Enqueue(b.Root, (float)ComputeMaxGeometricDistanceToClosestCellSquared(b.Root, a));
+            queue.Enqueue(octreeA.Root, (float)ComputeMaxGeometricDistanceToClosestCellSquared(octreeA.Root, octreeB));
+            queue.Enqueue(octreeB.Root, (float)ComputeMaxGeometricDistanceToClosestCellSquared(octreeB.Root, octreeA));
 
             // Process each cell or triangle
             while (queue.Count > 0)
@@ -85,7 +123,7 @@ namespace OPS.Geometry
                 // Find the nearest cell in the other octree at the same level as measured by minimum distance
                 OctreeNode current = queue.Dequeue();
                 Octree currentTree = current.Owner;
-                Octree otherTree = (currentTree == a) ? b : a;
+                Octree otherTree = (currentTree == octreeA) ? octreeB : octreeA;
                 OctreeNode nearestCellOnOtherMesh = FindNearestCellToGivenCellAtSameLevel(current, otherTree);
 
                 // Compute the shortest and furthest distance between these cells
@@ -159,6 +197,7 @@ namespace OPS.Geometry
                 // If the current cell is a leaf, process its triangles and insert them into the queue
                 else if (current.IsLeaf())
                 {
+                    // Process each triangle that intersects this cell (including those wholly contained)
                     foreach (VoxelTriangle tri in current.Intersecting)
                     {
                         ProcessTriangle(tri, current, otherTree);
@@ -167,6 +206,7 @@ namespace OPS.Geometry
                 // If not a leaf, insert its sub-cells into the queue
                 else
                 {
+                    // Insert all this cell's children into the queue
                     foreach (OctreeNode child in current.Children)
                     {
                         // Only process cells containing triangles at any sub-level
@@ -183,6 +223,12 @@ namespace OPS.Geometry
             return Math.Sqrt(hausdorffDistanceSquared);
         }
 
+        /// <summary>
+        /// Take a triangle and check its surroundings for its minimum and maximum possible distance to something else, and enqueue it if it isn't ruled out
+        /// </summary>
+        /// <param name="tri">The voxel triangle getting processed</param>
+        /// <param name="current">The cell which contains this triangle</param>
+        /// <param name="otherTree">The entire other octree from the other mesh which this triangle is not a part of</param>
         private void ProcessTriangle(VoxelTriangle tri, OctreeNode current, Octree otherTree)
         {
             // Compute the barycenter of this triangle
@@ -240,27 +286,59 @@ namespace OPS.Geometry
             queue.Enqueue(voxelTriangle, (float)furthestDistanceSquared);
         }
 
+        /// <summary>
+        /// Finds the points at which the three triangle points are geometrically closest to the surrounding mesh from the opposite octree
+        /// </summary>
+        /// <param name="tri">The triangle which we are finding nearby points from</param>
+        /// <param name="otherTree">The entire other octree for the mesh which this triangle is not a part of</param>
+        /// <returns></returns>
         private BasePoint[] FindNearestBasePointsFromTriangle(VoxelTriangle tri, Octree otherTree)
         {
+            // Prepare a home for the three base points that will be found
             BasePoint[] basePoints = new BasePoint[3];
+
+            // Get the three vertices in this triangle from which we find their nearest base points
             Vertex[] vertices = tri.Triangle.Vertices();
+
+            // In order to traverse the octree for nearby cells to find the triangle on which the nearest base point will be determined to exist,
+            // we need to start somewhere close by traversing down to the triangle's wholly-containing cell
             OctreeNode last = otherTree.FollowPath(tri.TraversalPath);
 
+            // Find the nearest base point located on the other mesh to each of the tree triangle vertices from the triangle in this mesh
             basePoints[0] = FindNearestBasePointFromPoint(vertices[0].Position, otherTree, last, out last);
             basePoints[1] = FindNearestBasePointFromPoint(vertices[1].Position, otherTree, last, out last);
             basePoints[2] = FindNearestBasePointFromPoint(vertices[2].Position, otherTree, last, out last);
 
+            // Return the base points which are closest to each of the tree triangle vertices in the opposite mesh
             return basePoints;
         }
 
+        /// <summary>
+        /// Takes a point and looks around the octree for the other mesh, trying to find the geometrically closest point to this point in space on that other mesh
+        /// </summary>
+        /// <param name="point">The point in 3D space where we want the closest point on the other mesh</param>
+        /// <param name="otherTree">The entire other octree containing the other mesh triangles to which we are looking for the closest point</param>
+        /// <param name="startingOctreeNode">A shortcut octree cell where we can begin searching its surroundings</param>
+        /// <param name="lastOctreeNode">An octree node that gets mutated after the search is finished to be used as a refined shortcut in future nearby searches</param>
+        /// <returns>The base point where the triangle on the other mesh is closest to this point in 3D space</returns>
         private BasePoint FindNearestBasePointFromPoint(Vector3 point, Octree otherTree, OctreeNode startingOctreeNode, out OctreeNode lastOctreeNode)
         {
+            // Find the closest triangle to this requested point in 3D space
             VoxelTriangle triangle = (VoxelTriangle)otherTree.Closest(point, startingOctreeNode, out lastOctreeNode);
+
+            // Find the physical point on the triangle which is geometrically closest to the requested point
             Vector3 closestPoint = triangle.Triangle.ClosestPoint(point).Position;
 
+            // Create a basepoint that wraps the point in space along with the triangle it's on
             return new BasePoint(closestPoint, triangle);
         }
 
+        /// <summary>
+        /// Gets the worst-case, furthest possible distance from one cell to any other closest cell that exists in the other octree
+        /// </summary>
+        /// <param name="sourceCell">The octree cell from which we are finding the closest other cell's furthest possible distance</param>
+        /// <param name="otherOctreeRoot">The root of the other octree which must be traversed to find the closest cell to the given one</param>
+        /// <returns>A distance between the two cells from furthest corner to furthest corner, squared</returns>
         private double ComputeMaxGeometricDistanceToClosestCellSquared(OctreeNode sourceCell, Octree otherOctreeRoot)
         {
             // Find the nearest cell in the other mesh's octree at the same level
@@ -270,33 +348,55 @@ namespace OPS.Geometry
             return BoundingBoxExtensions.FurthestDistanceSquared(nearestCell.Bounds, sourceCell.Bounds);
         }
 
+        /// <summary>
+        /// Looks through neighboring cells in the opposite octree at the same depth and gets which cell is closest
+        /// </summary>
+        /// <param name="sourceCell">The cell from which we are looking for the closest neighbor</param>
+        /// <param name="otherOctreeRoot">The root of the other octree so it can look for neighbors to the given cell at its depth</param>
+        /// <returns>The cell that is found to be closest to the given cell in the other octree</returns>
         private OctreeNode FindNearestCellToGivenCellAtSameLevel(OctreeNode sourceCell, Octree otherOctreeRoot)
         {
+            // Start with the biggest possible distance that represents the shortest distance to the queried cells so far
             double minDistanceSquared = Double.MaxValue;
+
+            // Transient result for the closest cell encountered so far, which eventually gets returned
             OctreeNode currentClosest = null;
 
+            // The distance squared of each node the search encounters
+            // This is set each time when checking if this distance is shorter than the current shortest, so it can then go and update the shortest without calculating the value again
             double currentDistanceSquared = 0;
 
+            // Determines if the node should be searched inside of, since it may already be further than the current closest
             Func<OctreeNode, bool> shouldSearchNode = (node =>
             {
+                // Find the distance from the given cell to this current node being traversed and cache that result in case it is indeed shorter, so it can be used in the update function
                 currentDistanceSquared = BoundingBoxExtensions.DistanceSquared(sourceCell.Bounds, node.Bounds);
+
+                // Return true if his node's distance is shorter than the current shortest and without going deeper than this cell
                 return currentDistanceSquared < minDistanceSquared && (node.Depth <= sourceCell.Depth || sourceCell.IsLeaf());
             });
 
+            // Update the shortest distance to a smaller value if this cell's distance is the right depth
             Action<OctreeNode> update = (node => {
                 if (node.Depth == sourceCell.Depth || node.IsLeaf())
                 {
+                    // Update the current closest distance and the current closest cell node
                     minDistanceSquared = currentDistanceSquared;
                     currentClosest = node;
                 }
             });
 
+            // Traverse through the other octree in order to find the closest other cell at the correct depth
             otherOctreeRoot.Visit(update, shouldSearchNode);
             
+            // Return the closest cell that was found to the given one from the other tree
             return currentClosest;
         }
     }
 
+    /// <summary>
+    /// Wrapper for a base point that holds the 3D position and the triangle the base point lies on
+    /// </summary>
     struct BasePoint
     {
         public Vector3 Position;
