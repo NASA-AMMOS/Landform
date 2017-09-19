ad1a2bca0c05d7f85fe5ec67114c426d764aa264
Schibler <thomas.schibler@jpl.nasa.gov>
2017-09-18 17:03:30 -0700

    Refactored to create node/edge classes for mesh representation

---
+++ Geometry/Geometry/VertexNode.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using OPS.Geometry;
+
+namespace OPS.Geometry
+{
+    /// <summary>
+    /// Stores a vertex with its associated error matrix, edges, and flags
+    /// </summary>
+    public class VertexNode
+    {
+        public Vertex Vert;
+        public Matrix Q;
+        public List<Edge> AdjacentEdges;
+        public bool IsOnPerimeter;
+        public bool IsTouchable;
+        public bool IsActive;
+        public int ID;
+
+        public VertexNode(Vertex vert, int id)
+        {
+            this.Vert = vert;
+            this.ID = id;
+            this.Q = new Matrix();
+            this.AdjacentEdges = new List<Edge>();
+            this.IsOnPerimeter = false;
+            this.IsTouchable = true;
+            this.IsActive = true;
+        }
+
+        public VertexNode(Vertex vert, int id, Matrix Q, List<Edge> adjacentEdges, bool isOnPerimeter, bool isTouchable)
+        {
+            this.Vert = vert;
+            this.ID = id;
+            this.Q = Q;
+            this.AdjacentEdges = adjacentEdges;
+            this.IsOnPerimeter = isOnPerimeter;
+            this.IsTouchable = isTouchable;
+            this.IsActive = true;
+        }
+
+        public static bool operator <(VertexNode v1, VertexNode v2)
+        {
+            return v1.ID < v2.ID;
+        }
+
+        public static bool operator >(VertexNode v1, VertexNode v2)
+        {
+            return v1.ID > v2.ID;
+        }
+    }
+}
