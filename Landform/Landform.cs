f558eef9ee9ca357a8390e30bc2ece1c092a227a
menzies <menzies@jpl.nasa.gov>
2017-10-10 15:49:22 -0700

    Reverted changes to landform.cs

--- Landform/Landform.cs
+++ Landform/Landform.cs
@@ -10,14 +10,13 @@ using OPS.Cloud;
 using OPS.Pipeline;
 using log4net;
 using OPS.Util;
-using System.Diagnostics;
 
 namespace Landform
 {
     class Landform
     {
         static ILog logger = LogManager.GetLogger(typeof(Landform));
-        
+
         /// <summary>
         /// The start of everything
         /// </summary>
@@ -25,23 +24,15 @@ namespace Landform
         /// <returns></returns>
         static int Main(string[] args)
         {
-            Mesh a = Mesh.Load(@"C:\Users\kchamber.JPL\Desktop/mars_original.obj");
-            Mesh b = Mesh.Load(@"C:\Users\kchamber.JPL\Desktop/mars_decimated.obj");
+            Config.ApplicationConfigFolder = ".landform";
+            // Enable logging
+            log4net.Config.XmlConfigurator.Configure();
+            // Register filetype handlers
+            new OpenInventorSerializer().Register();
 
-            //Mesh a = Mesh.Load(@"C:\Users\kchamber.JPL\Desktop/bunny.obj");
-            //Mesh b = Mesh.Load(@"C:\Users\kchamber.JPL\Desktop/bunny_decimated.obj");
-
-            //Mesh a = Mesh.Load(@"C:\Users\kchamber.JPL\Desktop/smooth_suzanne.obj");
-            //Mesh b = Mesh.Load(@"C:\Users\kchamber.JPL\Desktop/rough_suzanne.obj");
-
-            HausdorffDistance hausdorff = new HausdorffDistance();
-            double newHausdorff = hausdorff.Calculate(a, b, 1);
-            Console.WriteLine("New: " + newHausdorff);
-
-            double oldHausdorff = MeshLab.BidirectionalHausdorffDistance(a, b).Max;
-            Console.WriteLine("Old: " + oldHausdorff);
-
-            return 0;
+            // Parse command line arguments
+            int returnCode = Commands.RunFromCommandline(args);
+            return returnCode;
         }
     }
 }
