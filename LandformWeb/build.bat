3c1902813b409a24fe017f25be90aa5e985aaaf7
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2018-07-09 15:51:15 -0700

    npm install in client automatically

--- LandformWeb/build.bat
+++ LandformWeb/build.bat
@@ -1,9 +1,6 @@
 del landformweb.zip
 CALL npm install
 if %errorlevel% neq 0 exit /b %errorlevel%
-cd client
-CALL npm install
-cd ..
 CALL npm run build
 if %errorlevel% neq 0 exit /b %errorlevel%
 zip -r landformweb.zip . -x node_modules/* -x client/node_modules*
