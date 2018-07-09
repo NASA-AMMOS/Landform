d00a40674d3351a31a607e5a66c34a1a51675d50
menzies <menzies@jpl.nasa.gov>
2018-07-09 14:14:42 -0700

    Updated build scripts

--- LandformWeb/build.bat
+++ LandformWeb/build.bat
@@ -1,3 +1,7 @@
 del landformweb.zip
+CALL npm install
+if %errorlevel% neq 0 exit /b %errorlevel%
 CALL npm run build
+if %errorlevel% neq 0 exit /b %errorlevel%
 zip -r landformweb.zip . -x node_modules/* -x client/node_modules*
+if %errorlevel% neq 0 exit /b %errorlevel%
--- LandformWeb/deploy.bat
+++ LandformWeb/deploy.bat
@@ -1,2 +1,3 @@
 CALL build.bat
+if %errorlevel% neq 0 exit /b %errorlevel%
 eb deploy landformweb --profile %1
