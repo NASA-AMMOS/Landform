7cb4a976080855ba3dde95e57d2488622d77349f
menzies <menzies@jpl.nasa.gov>
2018-07-09 14:53:45 -0700

    Fixed bug in LandformWeb build script

--- LandformWeb/build.bat
+++ LandformWeb/build.bat
@@ -1,6 +1,9 @@
 del landformweb.zip
 CALL npm install
 if %errorlevel% neq 0 exit /b %errorlevel%
+cd client
+CALL npm install
+cd ..
 CALL npm run build
 if %errorlevel% neq 0 exit /b %errorlevel%
 zip -r landformweb.zip . -x node_modules/* -x client/node_modules*
