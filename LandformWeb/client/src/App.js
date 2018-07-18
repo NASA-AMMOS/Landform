e70308da53d306e7252f6728b2c3d054b1e7fe65
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2018-07-18 14:16:02 -0700

    minor cleanups in client code

--- LandformWeb/client/src/App.js
+++ LandformWeb/client/src/App.js
@@ -7,18 +7,19 @@ import Documentation from './Documentation';
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <h1 className="App-title">Landform Web Console</h1>
+      <h1 className="App-title">Landform</h1>
     </header>
-    <p className="App-intro">
-      <a href="/auth/sso">Login (SSO)</a><br />
-      <Link to="/ldapLogin">Login (LDAP)</Link> <br />
-      <a href="/auth/logout">Logout</a><br />
-      <a href="/auth/apiToken">API token</a><br />
+    <div className="App-intro">
+      <a href="/auth/sso">Login (SSO)</a><br/>
+      <Link to="/ldapLogin">Login (LDAP)</Link> <br/>
+      <a href="/auth/logout">Logout</a><br/>
+      <a href="/auth/apiToken">API token</a><br/>
       <Link to="/documentation">Documentation</Link>
-    </p>
+    </div>
     <div className="App-body">
-      <Route path="/ldapLogin" component={LdapLogin} />
-      <Route path="/documentation" component={Documentation} />
+      <Route path="/ldapLogin" component={LdapLogin}/>
+      <Route path="/documentation" component={Documentation}/>
+      <Route exact path="/" component={Documentation}/>
     </div>
   </div>
 );
