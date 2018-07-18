39f5ffcf4b76a498909a6c3e1ea93b8f09628101
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2018-07-18 13:09:10 -0700

    remove bogus error handlers

--- LandformWeb/server.js
+++ LandformWeb/server.js
@@ -44,26 +44,6 @@ require('./config/routes')(app, config, passport);
 // Configure api
 require('./config/api')(app);
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use((err, req, res) => {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err,
-    });
-  });
-}
-// production error handler
-// no stacktraces leaked to user
-app.use((err, req, res) => {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {},
-  });
-});
 
 app.listen(config.app.port, () => {
   console.log(`Express server listening on port ${config.app.port}`);
