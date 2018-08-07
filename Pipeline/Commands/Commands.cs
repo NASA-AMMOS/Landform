3ed84bc02994b12c095b2930f58880c292f65160
Bob Crocco <bob.crocco@jpl.nasa.gov>
2018-08-06 18:43:44 -0700

    stub for MSL.Texture

--- Pipeline/Commands/Commands.cs
+++ Pipeline/Commands/Commands.cs
@@ -30,7 +30,8 @@ namespace OPS.Pipeline
                                                              LegacyToTile3DOptions,
                                                              AlignSceneOptions,
                                                              CreateCloudTemplatesOptions,
-                                                             TileLocalMeshOptions
+                                                             TileLocalMeshOptions,
+                                                             TextureMeshOptions
                                                              >(args)
               .MapResult(
                 (AlignmentWorkerOptions opts) => new AlignmentWorker().Run(),
@@ -46,6 +47,7 @@ namespace OPS.Pipeline
                 (AlignSceneOptions opts) => new AlignScene(opts).Run(),
                 (CreateCloudTemplatesOptions opts) => new CreateCloudTemplates(opts).Run(),
                 (TileLocalMeshOptions opts) => new TileLocalMesh(opts).Run(),
+                (TextureMeshOptions opts) => new TextureMesh(opts).Run(),
                 errs => 1);
         }
     }
---
+++ Pipeline/Commands/TextureMesh.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using OPS.Plumbing;
+using log4net;
+
+namespace OPS.Pipeline
+{
+    [Verb("MSL.Texture", HelpText = "generate textures for a terrain mesh")]
+    public class TextureMeshOptions
+    {
+    }
+
+    class TextureMesh 
+    {
+        static ILog logger = LogManager.GetLogger(typeof(TextureMesh));
+
+        public TextureMeshOptions options;
+
+        public TextureMesh(TextureMeshOptions opts)
+        {
+            this.options = opts;
+        }
+
+        public int Run()
+        {
+            logger.Info("Texturing mesh...");
+            return 0;
+        }
+    }
+}
