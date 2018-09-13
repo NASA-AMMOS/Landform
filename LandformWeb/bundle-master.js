f25139f01dd9ad50c3cd50258ade9f6c9fd7e20f
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2018-09-12 20:06:39 -0700

    fix bundle-master script

--- LandformWeb/bundle-master.js
+++ LandformWeb/bundle-master.js
@@ -2,17 +2,13 @@ const fs = require('fs-extra');
 const path = require('path');
 const Zip = require('@jpl/adm-zip');
 
-const bundle = require('./config').app.bundle;
+const config = require('./config');
+
+const bundle = config.app.bundle;
+const binDir = config.app.binDir;
 
 //the "bundle" script in package.json creates the app zip bundle as a git archive of HEAD
 //our task here is to add the bin/ subtree to it
-
-const dbg = process.argv.slice(2).some(a => {
-  const la = a.toLowerCase();
-  return la === '-debug' || la === '--debug' || la === '--debug=true';
-});
-
-const binDir = path.join('..', 'TilingServer', 'bin', dbg ? 'Debug' : 'Release');
 
 //copying only selected files results in a zip that is 100x smaller (1.7M vs 170M)
 const files = [
@@ -30,15 +26,12 @@ if (fs.pathExistsSync(bundle) && fs.pathExistsSync(binDir)) {
 
   const z = new Zip(bundle);
 
-  const src = fs.realpathSync(path.join(process.cwd(), binDir));
-
   let nextTmpDir = 0, tmpDir = null;
   do { tmpDir = path.join('tmp', `bundle-master${nextTmpDir++}`); } while (fs.pathExistsSync(tmpDir));
   fs.ensureDirSync(tmpDir);
 
   try {
-    //fs.copySync(src, tmpDir);
-    files.forEach(f => fs.copySync(path.join(src, f), path.join(tmpDir, f)));
+    files.forEach(f => fs.copySync(path.join(binDir, f), path.join(tmpDir, f)));
     z.addLocalFolder(tmpDir, 'bin');
   } finally { fs.remove(tmpDir); }
 
