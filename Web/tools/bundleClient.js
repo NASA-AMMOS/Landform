2fe4f90d00ffdc0fee174d6047a003bd2e81e534
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2018-11-02 16:11:50 -0700

    overwrite bundle zips by default

--- Web/tools/bundleClient.js
+++ Web/tools/bundleClient.js
@@ -3,16 +3,22 @@ const path = require('path');
 const Zip = require('@jpl/adm-zip');
 
 const bundle = require('../config').app.bundle;
+const { prompt, hasFlag } = require('./toolUtil');
 
 //the "bundle" script in package.json creates the app zip bundle as a git archive of HEAD
 //our task here is to add the client/build/ subtree to it
 
-const dir = path.join('client', 'build');
+const clientDir = path.join('client', 'build');
 
-if (fs.pathExistsSync(bundle) && fs.pathExistsSync(dir)) {
-  console.log(`adding '${dir}' to '${bundle}'`);
+function zipIt() {
+  console.log(`adding '${clientDir}' to '${bundle}'`);
   const z = new Zip(bundle);
-  z.addLocalFolder(dir, dir);
+  z.addLocalFolder(clientDir, clientDir);
   z.writeZip(bundle);
   //z.getEntries().forEach(e => console.log(e.entryName));
-} else console.log(`cannot add '${dir}' to '${bundle}', one or both missing (run 'npm build-client')`);
+}
+
+if (fs.pathExistsSync(clientDir)) {
+  if (!fs.pathExistsSync(bundle) || hasFlag('overwrite')) zipIt();
+  else prompt('bundleClient', `overwrite ${bundle}`).then(ok => { if (ok) { fs.removeSync(bundle); zipIt(); } });
+} else console.log(`'${clientDir}' not found`);
