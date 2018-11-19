78cd69d3975b677a8e6cfa7cfab0d0e1115e3f93
Bob Crocco <bob.crocco@jpl.nasa.gov>
2018-11-19 15:49:02 -0800

    change location of red bucket for workers (unused startworker this getting the red profile, all newly created workers missing it)

--- Pipeline/TileServer/Commands/StartWorker.cs
+++ Pipeline/TileServer/Commands/StartWorker.cs
@@ -62,15 +62,6 @@ namespace OPS.Pipeline.TileServer
             : base(options, TileServerConfig.Instance.VenueName, TileServerConfig.Instance.Profile)
         {
             this.options = options;
-
-            //MSL specific: this project does not hold its images within the same s3 bucket as the project
-            //future projects  are expected to be within the same bucket
-            var config = TileServerConfig.Instance;
-            if (!string.IsNullOrEmpty(config.MSLICEProfile) && !string.IsNullOrEmpty(config.MSLICES3Url) &&
-                OPS.Cloud.Credentials.Exists(config.MSLICEProfile))
-            {
-                this.AddProfile(config.MSLICES3Url, config.MSLICEProfile);
-            }
         }
 
         public int Run()
@@ -365,6 +356,16 @@ namespace OPS.Pipeline.TileServer
             var pipeline = new PipelineCore(options,
                                             TileServerConfig.Instance.VenueName, TileServerConfig.Instance.Profile,
                                             logger: Logger); //all threads share the same logger which is MT safe
+
+            //MSL specific: this project does not hold its images within the same s3 bucket as the project
+            //future projects  are expected to be within the same bucket
+            var config = TileServerConfig.Instance;
+            if (!string.IsNullOrEmpty(config.MSLICEProfile) && !string.IsNullOrEmpty(config.MSLICES3Url) &&
+                OPS.Cloud.Credentials.Exists(config.MSLICEProfile))
+            {
+                pipeline.AddProfile(config.MSLICES3Url, config.MSLICEProfile);
+            }
+
             var cloud = new TileServerCloud(pipeline, initQueues: true, initTables: false, quiet: true);
             var workerQueue = cloud.WorkerQueue;
 
