05654bc4366ab7ec8985216ec28a4589c0a65c6c
Bob Crocco <bob.crocco@jpl.nasa.gov>
2018-12-12 14:40:54 -0800

    fix bug in border generation code (was only generating borders for a single pixel wide corners, not whole row or col at border width/height) honor the missing data pixel values in pds images and add them to mask, in cases where distortion was severe you could have missing data that extended beyond the hardcoded border pull borderwidth into constant

--- Pipeline/AlignmentServer/Commands/CreateMask.cs
+++ Pipeline/AlignmentServer/Commands/CreateMask.cs
@@ -1,6 +1,7 @@
 ﻿using OPS.Cloud;
 using OPS.Pipeline.TileServer;
 using OPS.Plumbing;
+using OPS.Imaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,20 +22,14 @@ namespace OPS.Pipeline.AlignmentServer
             Cloud = cloud;
         }
 
+        //settings
+        private static readonly int borderMaxWidth = 10;
+
         public void Process()
         {
             var imgRef = Message.Image;
             var img = Pipeline.Load(imgRef);
-
-            var mask = RoverMask.Build(img);
-            int borderPx = Math.Min(mask.Width / 2, 10);
-            for (int border = 0; border < borderPx; border++)
-            {
-                mask[0, border, 0] = 0;
-                mask[0, 0, border] = 0;
-                mask[0, mask.Height - 1 - border, 0] = 0;
-                mask[0, 0, mask.Width - 1 - border] = 0;
-            }
+            Imaging.Image mask = MakeMask(img);
             var maskProd = new PngDataProduct(mask);
             Pipeline.Save(Message.Project, maskProd);
 
@@ -45,5 +40,48 @@ namespace OPS.Pipeline.AlignmentServer
                 MaskGuid = maskProd.Guid
             });
         }
+
+        public static Imaging.Image MakeMask(Imaging.Image img)
+        {
+            var mask = RoverMask.Build(img);
+
+            //corrects for missing data, often caused by undistorting an image
+            PDSParser parser = new PDSParser(img.Metadata as PDSMetadata);
+            if (parser.HasMissingConstant)
+            {
+                float[] missingVal = parser.MissingConstant.Select(x => (float)x).ToArray();
+                for (int idxRow = 0; idxRow < img.Height; idxRow++)
+                {
+                    for (int idxCol = 0; idxCol < img.Width; idxCol++)
+                    {
+                        if (img.BandValuesEqual(idxRow, idxCol, missingVal))
+                        {
+                            mask[0, idxRow, idxCol] = 0;
+                        }
+                    }
+                }
+            }
+
+            int borderPx = Math.Min(mask.Width / 2, borderMaxWidth);
+            for (int border = 0; border < borderPx; border++)
+            {
+                //whole row
+                for(int idxCol=0;idxCol<mask.Width; idxCol++)
+                {
+                    mask[0, border, idxCol] = 0;
+                    mask[0, mask.Height - 1 - border, idxCol] = 0;
+                }
+
+                //whole column
+                for (int idxRow = 0; idxRow < mask.Height; idxRow++)
+                {
+                    mask[0, idxRow, border] = 0;
+                    mask[0, idxRow, mask.Width - 1 - border] = 0;
+                }
+            }
+
+            return mask;
+        }
+
     }
 }
