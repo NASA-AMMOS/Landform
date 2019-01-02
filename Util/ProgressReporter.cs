1ac5c5432d39d2bd42808032a8ee988157d9de58
Alex Menzies <menzies@jpl.nasa.gov>
2019-01-02 13:58:11 -0800

    Added a class for rate limited progress reporting

---
+++ Util/ProgressReporter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPS.Util
+{
+    public class ProgressReporter
+    {
+
+        public delegate void Report(int v);
+
+        DateTime lastReport = new DateTime();
+        Report ReportFunction;
+        int rate;
+        int? lastValue;
+
+        public ProgressReporter(int rate, Report f)
+        {
+            this.rate = rate;
+            this.ReportFunction = f;
+        }
+
+        public void Update(int value)
+        {
+            if(lastValue == null || lastValue.Value != value)
+            {
+                if((DateTime.Now - lastReport).TotalSeconds >= rate)
+                {
+                    lastReport = DateTime.Now;
+                    lastValue = value;
+                    ReportFunction(value);
+                }
+            }            
+        }
+    }
+}
