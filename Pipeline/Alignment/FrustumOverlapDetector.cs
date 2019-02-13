6404854bb43c9716f3d8310a2888ccbed575bcf8
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-02-12 18:06:04 -0800

    more spew for FrustumOverlapDetector

--- Pipeline/Alignment/FrustumOverlapDetector.cs
+++ Pipeline/Alignment/FrustumOverlapDetector.cs
@@ -16,12 +16,16 @@ namespace OPS.Pipeline
     {
         public FrustumOverlapDetector(PipelineCore pipeline) : base(pipeline) { }
 
-        public void MakeHulls(AlignmentScene scene)
+        public int MakeHulls(AlignmentScene scene)
         {
             Dictionary<SceneNode, ConvexHull> worldHull = new Dictionary<SceneNode, ConvexHull>();
-            Action<SceneNode> collect = null;
-            collect = (node) =>
+
+            double lastSpew = UTCTime.Now();
+            int numNodes = 0, existingHulls = 0, paramsHulls = 0, imageHulls = 0, unionHulls = 0;
+            void make(SceneNode node)
             {
+                numNodes++;
+
                 if (node.HasComponent<NodeImageUrl>())
                 {
                     if (node.ChildCount > 0)
@@ -48,20 +52,39 @@ namespace OPS.Pipeline
                                 chc.Hull = ConvexHull.FromParams((CameraModel)JsonHelper.FromJson(obs.CameraModel),
                                                                  obs.Width, obs.Height);
                                 added = true;
+                                paramsHulls++;
                             }
                         }
                         catch { }
                         if (!added)
                         {
                             chc.Hull = ConvexHull.FromImage(pipeline.LoadImage(imgUrl));
+                            imageHulls++;
                         }
                     }
+                    else
+                    {
+                        existingHulls++;
+                    }
+                }
+
+                double now = UTCTime.Now();
+                if (now - lastSpew > 10)
+                {
+                    pipeline.LogInfo("processed {0} nodes, {1} had hulls, made {2} hulls from params, {3} from images, "
+                                     + " {4} from union of child hulls",
+                                     numNodes, existingHulls, paramsHulls, imageHulls, unionHulls);
+                    lastSpew = now;
+                }
+
+                if (node.IsLeaf)
+                {
                     return;
                 }
 
                 foreach (var child in node.Children)
                 {
-                    collect(child);
+                    make(child);
                 }
 
                 if (!node.HasComponent<NodeConvexHull>())
@@ -70,60 +93,90 @@ namespace OPS.Pipeline
                     foreach (var child in node.Children)
                     {
                         var hull = child.GetComponent<NodeConvexHull>();
-                        if (hull != null)
-                        {
-                            var ut = child.GetOrAddComponent<NodeUncertainTransform>();
-                            childHulls.Add(ConvexHull.Transformed(hull.Hull, ut.UncertainTransform));
-                        }
-                        else
-                        {
-                            Debug.Write("no hulls");
-                        }
+                        Debug.Assert(hull != null);
+                        var ut = child.GetOrAddComponent<NodeUncertainTransform>();
+                        childHulls.Add(ConvexHull.Transformed(hull.Hull, ut.UncertainTransform));
                     }
                     if (childHulls.Count > 0)
                     {
                         var chc = node.AddComponent<NodeConvexHull>();
                         chc.Hull = ConvexHull.Union(childHulls.ToArray());
+                        unionHulls++;
                     }
                 }
-            };
-            collect(scene.Root);
+            }
+
+            make(scene.Root);
+
+            pipeline.LogInfo("{0} total nodes, {1} had hulls, made {2} hulls from params, {3} from images, "
+                             + " {4} from union of child hulls",
+                             numNodes, existingHulls, paramsHulls, imageHulls, unionHulls);
+
+            return numNodes;
         }
         
-        public void Detect(AlignmentScene scene, bool allowInternalOverlaps=true)
+        public void Detect(AlignmentScene scene, bool allowInternalOverlaps = true)
         {
-            // Initialize - make sure ImageToNode is up to date and all nodes have hulls
+            pipeline.LogInfo("detecting overlaps, {0}allowing internal overlaps", allowInternalOverlaps ? "" : "not ");
+
+            // make sure ImageToNode is up to date and all nodes have hulls
             MakeHulls(scene);
-           
-            Func<SceneNode, SceneNode, bool> overlaps = (node, other) =>
+
+            double lastSpew = UTCTime.Now();
+            int overlapChecks = 0, processedPairs = 0, overlappingLeaves = 0, processedNodes = 0;
+            HashSet<URLPair> unique = new HashSet<URLPair>();
+
+            void spewMaybe(bool force = false)
             {
+                double now = UTCTime.Now();
+                if (force || (now - lastSpew > 10))
+                {
+                    pipeline.LogInfo("found {0} unique overlaps ({1} checks), {2} overlapping leaf pairs, " +
+                                     "processed {3} pairs, {4} nodes",
+                                     unique.Count, overlapChecks, overlappingLeaves, processedPairs, processedNodes);
+                    lastSpew = now;
+                }
+            }
+
+            bool doesOverlap(SceneNode node, SceneNode other)
+            {
+                overlapChecks++;
+
                 if (!node.HasComponent<NodeConvexHull>()) return false;
                 if (!other.HasComponent<NodeConvexHull>()) return false;
+
                 var nodeHull = node.GetComponent<NodeConvexHull>().Hull;
                 var otherHull = other.GetComponent<NodeConvexHull>().Hull;
 
                 var nodeToOther = node.GetOrAddComponent<NodeUncertainTransform>().To(other);
                 var thisInOther = ConvexHull.Transformed(nodeHull, nodeToOther);
-                return thisInOther.Intersects(otherHull);
-            };
 
-            HashSet<URLPair> unique = new HashSet<URLPair>();
+                return thisInOther.Intersects(otherHull);
+            }
+
             void processPairwise(SceneNode one, SceneNode two)
             {
-                // Debug.Assert(overlaps(ci, cj));
+                processedPairs++;
+
+                // Debug.Assert(doesOverlap(ci, cj));
 
                 if (one.HasComponent<NodeImageUrl>() && two.HasComponent<NodeImageUrl>())
                 {
                     unique.Add(new URLPair(one.GetComponent<NodeImageUrl>().Url, two.GetComponent<NodeImageUrl>().Url));
+                    overlappingLeaves++;
                 }
+
+                spewMaybe();
 
                 if (!one.IsLeaf)
                 {
                     SceneNode[] children = one.Children.ToArray();
                     for (int i = 0; i < children.Length; i++)
                     {
-                        if (!overlaps(children[i], two)) continue;
-                        processPairwise(children[i], two);
+                        if (doesOverlap(children[i], two))
+                        {
+                            processPairwise(children[i], two);
+                        }
                     }
                 }
                 else if (!two.IsLeaf)
@@ -133,33 +186,42 @@ namespace OPS.Pipeline
                     SceneNode[] children = two.Children.ToArray();
                     for (int i = 0; i < children.Length; i++)
                     {
-                        if (!overlaps(children[i], one)) continue;
-                        processPairwise(children[i], one);
+                        if (doesOverlap(children[i], one))
+                        {
+                            processPairwise(children[i], one);
+                        }
                     }
                 }
-                // else if (one.IsLeaf && two.IsLeaf) {}
             }
 
             void processNode(SceneNode node)
             {
+                processedNodes++;
+                spewMaybe();
                 SceneNode[] children = node.Children.ToArray();
                 for (int i = 0; i < children.Length; i++)
                 {
-                    var ci = children[i];
+                    var child = children[i];
                     for (int j = i + 1; j < children.Length; j++)
                     {
-                        var cj = children[j];
-                        if (!overlaps(ci, cj)) continue;
-                        processPairwise(ci, cj);
+                        if (doesOverlap(child, children[j]))
+                        {
+                            processPairwise(child, children[j]);
+                        }
                     }
 
                     if (allowInternalOverlaps)
                     {
-                        processNode(ci);
+                        processNode(child);
                     }
                 }
             }
             processNode(scene.Root);
+
+            spewMaybe(true);
+
+            pipeline.LogInfo("done detecting overlaps, {0} unique overlaps", unique.Count);
+
             scene.Overlaps = unique;
         }
 
