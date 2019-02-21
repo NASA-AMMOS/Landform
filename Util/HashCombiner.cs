4dc981339a002b885d270c0cccd7908c1028ec8b
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-02-21 11:22:02 -0800

    tweak hashcombiner api

--- Util/HashCombiner.cs
+++ Util/HashCombiner.cs
@@ -5,15 +5,20 @@ using System.Linq;
 
 namespace OPS.Util
 {
+    //this is available in .NET Core 2.1+
     public class HashCombiner
     {
-        //this is available stock in .NET Core 2.1+
         public static int Combine(object a, object b)
+        {
+            return Combine(a.GetHashCode(), b.GetHashCode());
+        }
+
+        public static int Combine(int a, int b)
         {
             //https://stackoverflow.com/questions/1646807/quick-and-simple-hash-code-combinations
             int hash = 17;
-            hash = hash * 31 + a.GetHashCode();
-            hash = hash * 31 + b.GetHashCode();
+            hash = hash * 31 + a;
+            hash = hash * 31 + b;
             return hash;
         }
     }
