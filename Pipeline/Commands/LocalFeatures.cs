a8e27fcda6774e9530237a57ef5811d441769c2d
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-03-05 09:44:25 -0800

    add --cloud, --histogrambucketsize, --tallyexisting to local-features

--- Pipeline/Commands/LocalFeatures.cs
+++ Pipeline/Commands/LocalFeatures.cs
@@ -40,54 +40,66 @@ namespace OPS.Pipeline
         [Option(HelpText = "Debug image format, e.g. png, jpg, help for list", Default = "png")]
         public string ImageFormat { get; set; }
 
+        [Option(HelpText = "Histogram bucket size", Default = 50)]
+        public int HistogramBucketSize { get; set; }
+
+        [Option(HelpText = "Include existing products in histogram", Default = false)]
+        public bool TallyExisting { get; set; }
+
         [Option(HelpText = "Hide progress", Default = false)]
         public bool NoProgress { get; set; }
+
+        [Option(HelpText = "Operate on cloud data", Default = false)]
+        public bool Cloud { get; set; }
     }
 
-    public class LocalFeatures : LocalPipeline
+    public class LocalFeatures
     {
         private LocalFeaturesOptions options;
+        private PipelineCore pipeline;
+        private string imageDir;
+        private string imageExt;
 
-        public LocalFeatures(LocalFeaturesOptions options) : base(options)
+        public LocalFeatures(LocalFeaturesOptions options)
         {
             this.options = options;
+            if (options.Cloud)
+            {
+                this.pipeline = new CloudPipeline(options, initQueues: false);
+            }
+            else
+            {
+                this.pipeline = new LocalPipeline(options);
+            }
         }
 
         public int Run()
         {
-            var project = Project.Find(this, options.ProjectName);
+            var project = Project.Find(pipeline, options.ProjectName);
 
             if (project == null)
             {
-                LogError("project \"{0}\" not found", options.ProjectName);
+                pipeline.LogError("project \"{0}\" not found", options.ProjectName);
                 return 1;
             }
 
-            string imagePath = options.ImageOutputFolder;
-            if (!string.IsNullOrEmpty(imagePath))
-            {
-                imagePath = StringHelper.NormalizeUrl(imagePath, "file://");
-            }
-            else
-            {
-                imagePath = GetStorageUrl("alignment/FeatureProducts", project.Name);
-            }
-            imagePath += "/";
+            imageDir =
+                pipeline.GetLocalDebugFolder(options.ImageOutputFolder, "alignment/FeatureProducts", project.Name);
 
-            string imageExt = null;
             if (options.WriteFeatureImages)
             {
-                imageExt = ImageSerializers.Instance.CheckFormat(options.ImageFormat, this);
+                imageExt = ImageSerializers.Instance.CheckFormat(options.ImageFormat, pipeline);
                 if (imageExt == null)
                 {
                     return 0;
                 }
+                pipeline.LogInfo("writing {0} feature images to {1}", imageExt, imageDir);
             }
 
-            var frameCache = new FrameCache(this, options.ProjectName);
+            var frameCache = new FrameCache(pipeline, options.ProjectName);
             frameCache.Preload(loadTransforms: false);
 
-            var observationCache = new ObservationCache(this, options.ProjectName);
+            var observationCache = new ObservationCache(pipeline, options.ProjectName);
             observationCache.Preload(obs => obs.UseForReconstruction);
 
             var imageType = ObservationType.Image.ToString();
@@ -103,12 +115,11 @@ namespace OPS.Pipeline
             }
 
             int no = obsForFrame.Values.Count(obsGroup => obsGroup.Any(obs => obs.ObservationType == imageType));
-            LogInfo("computing {0} features for {1} reconstruction images", options.DetectorType, no);
+            pipeline.LogInfo("computing {0} features for {1} reconstruction images", options.DetectorType, no);
 
-            FeatureDetector detector = new FeatureDetector(this, options.DetectorType, options.MaxFeaturesPerImage,
+            FeatureDetector detector = new FeatureDetector(pipeline, options.DetectorType, options.MaxFeaturesPerImage,
                                                            options.MinFeatureSize);
 
-            int bucketSize = 50;
             var histogram = new ConcurrentDictionary<int, int>();
             double startSec = UTCTime.Now();
             int nc = 0, ne = 0, nf = 0, np = 0;
@@ -122,22 +133,40 @@ namespace OPS.Pipeline
                     var imageObs = observations.Find(obs => obs.ObservationType == imageType);
                     if (imageObs != null)
                     {
+                        var maskObs = observations.Find(obs => obs.ObservationType == maskType &&
+                                                        obs.Width == imageObs.Width && obs.Height == imageObs.Height);
+                        var maskUrl = maskObs != null ? maskObs.Url : null;
+
                         if (imageObs.FeaturesGuid != null && imageObs.FeaturesGuid != Guid.Empty)
                         {
                             Interlocked.Increment(ref ne);
                             if (!options.RedoFeatures)
                             {
-                                LogVerbose("not recomputing features for observation {0}", imageObs.Name);
+                                pipeline.LogVerbose("not recomputing features for observation {0}", imageObs.Name);
+                                if (options.WriteFeatureImages || options.TallyExisting)
+                                {
+                                    var product =
+                                        pipeline.GetDataProduct<DetectedFeatures>(project.ProductPath,
+                                                                                  imageObs.FeaturesGuid, project.Name);
+                                    if (options.WriteFeatureImages)
+                                    {
+                                        WriteFeatureImage(product, maskUrl, imageObs.Name);
+                                    }
+                                    if (options.TallyExisting)
+                                    {
+                                        AddToHistogram(product, histogram);
+                                    }
+                                }
                                 return;
                             }
                             else
                             {
-                                LogVerbose("recomputing features for observation {0}", imageObs.Name);
+                                pipeline.LogVerbose("recomputing features for observation {0}", imageObs.Name);
                             }
                         }
                         else
                         {
-                            LogVerbose("computing features for observation {0}", imageObs.Name);
+                            pipeline.LogVerbose("computing features for observation {0}", imageObs.Name);
                         }
                         
                         Interlocked.Increment(ref nf);
@@ -145,33 +174,22 @@ namespace OPS.Pipeline
                         
                         if (!options.NoProgress)
                         {
-                            LogInfo("computing features for {0} images in parallel, completed {1}/{2}", np, nc, no);
+                            pipeline.LogInfo("computing features for {0} images in parallel, completed {1}/{2}",
+                                             np, nc, no);
                         }
-                        
-                        var maskObs =
-                            observations.Find(obs => obs.ObservationType == maskType &&
-                                              obs.Width == imageObs.Width && obs.Height == imageObs.Height);
-                        var maskUrl = maskObs != null ? maskObs.Url : null;
                         
                         var result = detector.Detect(imageObs.Url, maskUrl, project.Name, project.ProductPath);
                         if (result != null)
                         {
-                            SaveDataProduct(project.ProductPath, result, project.Name);
+                            pipeline.SaveDataProduct(project.ProductPath, result, project.Name);
                             imageObs.FeaturesGuid = result.Guid;
-                            imageObs.Save(this);
-                            int bucket = result.Features.Length / bucketSize;
-                            histogram.AddOrUpdate(bucket, _ => 1, (_, count) => count + 1);
+                            imageObs.Save(pipeline);
+                            AddToHistogram(result, histogram);
                         }
                         
                         if (options.WriteFeatureImages)
                         {
-                            var img = new Image(LoadImage(imageObs.Url));
-                            var mask = FeatureDetecting.MakeMask(this, maskUrl, img, imageObs.Name);
-                            img = FeatureDetecting.DrawFeatures(img, mask, result.Features);
-                            TemporaryFile.GetAndDelete(imageExt, tmpImage => {
-                                    img.Save<byte>(tmpImage);
-                                    SaveFile(tmpImage, imagePath + imageObs.Name + "-Features" + imageExt);
-                                });
+                            WriteFeatureImage(result, maskUrl, imageObs.Name);
                         }
 
                         Interlocked.Decrement(ref np);
@@ -182,13 +200,28 @@ namespace OPS.Pipeline
             
             foreach (var bucket in histogram.Keys.OrderBy(n => n))
             {
-                LogInfo("{0} images with {1} to {2} features", histogram[bucket],
-                        bucket * bucketSize, (bucket + 1) * bucketSize - 1);
+                pipeline.LogInfo("{0} images with {1} to {2} features", histogram[bucket],
+                                 bucket * options.HistogramBucketSize, (bucket + 1) * options.HistogramBucketSize - 1);
             }
-            LogInfo("processed {0} reconstruction images ({1:F3}s), computed features for {2} images ({3} existing)",
-                    nc, totalSec, nf, ne);
-
+            pipeline.LogInfo("processed {0} reconstruction images ({1:F3}s), computed features for {2} images " +
+                             "({3} existing)", nc, totalSec, nf, ne);
+            
             return 0;
+        }
+
+        private void AddToHistogram(DetectedFeatures product, ConcurrentDictionary<int, int> histogram)
+        {
+            int bucket = product.Features.Length / options.HistogramBucketSize;
+            histogram.AddOrUpdate(bucket, _ => 1, (_, count) => count + 1);
+        }
+
+        private void WriteFeatureImage(DetectedFeatures product, string maskUrl, string observationName)
+        {
+            var img = new Image(pipeline.LoadImage(product.ImageUrl)); //don't mutate original image
+            var mask = FeatureDetecting.MakeMask(pipeline, maskUrl, img, observationName);
+            img = FeatureDetecting.DrawFeatures(img, mask, product.Features);
+            PathHelper.EnsureExists(imageDir);
+            img.Save<byte>(string.Format("{0}{1}-Features{2}", imageDir, observationName, imageExt));
         }
     }
 }
