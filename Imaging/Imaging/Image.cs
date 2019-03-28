7a6531993d325032810acde42b48ee2d6892cfa3
Bob Crocco <bob.crocco@jpl.nasa.gov>
2019-03-28 13:45:04 -0700

    PR feedback: move colorize into image

--- Imaging/Imaging/Image.cs
+++ Imaging/Imaging/Image.cs
@@ -754,5 +754,52 @@ namespace OPS.Imaging
                 }
             }
         }
+
+        /// <summary>
+        /// converts the floating point values in the source image to colorized values. the previewBucketdistances
+        /// are the boundaries for the colors in colorsLowToHigh. There should be one more color than distance to catch
+        /// the distances that are larger than the final bucket cutoff
+        /// </summary>
+        /// <param name="img">single band source image</param>
+        /// <param name="colorCutoffValues">the floating point values that represent the upper bound of that color (eg. cutoffvalue 0.2, all values less than 0.2 get that value.</param>
+        /// <param name="colorsLowToHigh">colors intended to be paired with colorCutoffValues. each color in R, G, B order, range 0 to 1. Should be 1 more color than cutoff values as the upper-end catchall color (greater than the last color cutoff value)</param>
+        /// <param name="bgColor">color in R, G, B order, range 0 to 1</param>
+        /// <returns>3 band colorized image</returns>
+        static public Image ColorizeScalarImage(Image img, float[] colorCutoffValues, float[][] colorsLowToHigh, float[] bgColor)
+        {
+            if (img.Bands != 1)
+                throw new InvalidDataException("expecting a single band image to be colorized");
+
+            Image result = new Image(3, img.Width, img.Height);
+            result.CreateMask(true);
+
+            for (int idxRow = 0; idxRow < img.Height; idxRow++)
+            {
+                for (int idxCol = 0; idxCol < img.Width; idxCol++)
+                {
+                    if (img.IsInvalid(idxRow, idxCol))
+                    {
+                        result.SetBandValues(idxRow, idxCol, bgColor);
+                        continue;
+                    }
+
+                    float val = img[0, idxRow, idxCol];
+                    float[] color = colorsLowToHigh.Last(); //catchall for values > final cuttoff.
+                    for (int idxColor = 0; idxColor < colorCutoffValues.Length; idxColor++)
+                    {
+                        if (val < colorCutoffValues[idxColor])
+                        {
+                            color = colorsLowToHigh[idxColor];
+                            break;
+                        }
+                    }
+
+                    result.SetBandValues(idxRow, idxCol, color);
+                    result.SetMaskValue(idxRow, idxCol, false);
+                }
+            }
+
+            return result;
+        }
     }
 }
--- Pipeline/Commands/LocalObservationProducts.cs
+++ Pipeline/Commands/LocalObservationProducts.cs
@@ -341,7 +341,7 @@ namespace OPS.Pipeline
                                 deltaRangeImage.Save<float>(path + imageName + ".tif");
 
                                 Vector3 backgroundColor = new Vector3(0.9, 0.9, 0.9);
-                                Image deltaRangePreview = ColorizeFloatImage(deltaRangeImage.Decimated(4), previewDistanceBuckets, colors, backgroundColor);
+                                Image deltaRangePreview = Image.ColorizeScalarImage(deltaRangeImage.Decimated(4), previewDistanceBuckets, colors.Select(c => c.ToFloatArray()).ToArray(), backgroundColor.ToFloatArray());
                                 deltaRangePreview = StampLegend(deltaRangePreview, previewDistanceBuckets, colors, backgroundColor);
                                 deltaRangePreview.DeleteMask();
                                 deltaRangePreview.Save<byte>(pathPreview + imageName + ".png");
@@ -358,47 +358,7 @@ namespace OPS.Pipeline
 
             return 0;
         }
-
-        //converts the floating point values in the source image to colorized values. the previewBucketdistances
-        // are the boundaries for the colors in colorsLowToHigh. There should be one more color than distance to catch
-        // the distances that are larger than the final bucket cutoff
-        private Image ColorizeFloatImage(Image img, float[] previewDistanceBuckets, Vector3[] colorsLowToHigh, Vector3 bgColor)
-        {
-            Image result = new Image(3, img.Width, img.Height);
-            result.CreateMask(true);
-            
-            for(int idxRow = 0; idxRow < img.Height; idxRow++)
-            {
-                for (int idxCol = 0; idxCol < img.Width; idxCol++)
-                {
-                    if (img.IsInvalid(idxRow, idxCol))
-                    {
-                        result[0, idxRow, idxCol] = (float)bgColor.X;
-                        result[1, idxRow, idxCol] = (float)bgColor.Y;
-                        result[2, idxRow, idxCol] = (float)bgColor.Z;
-                        continue;
-                    }
-
-                    float val = img[0, idxRow, idxCol];
-                    Vector3 color = colorsLowToHigh.Last();
-                    for(int idxColor = 0; idxColor < previewDistanceBuckets.Length; idxColor++)
-                    {
-                        if (val < previewDistanceBuckets[idxColor])
-                        {
-                            color = colorsLowToHigh[idxColor];
-                            break;
-                        }
-                    }
-
-                    result[0, idxRow, idxCol] = (float)color.X;
-                    result[1, idxRow, idxCol] = (float)color.Y;
-                    result[2, idxRow, idxCol] = (float)color.Z;
-                    result.SetMaskValue(idxRow, idxCol, false);
-                }
-            }
-
-            return result;
-        }
+       
         
         Rgb ToEmguColor(Vector3 color)
         {
