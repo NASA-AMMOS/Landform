1d99c3340ccd136a33d99d0ed48ca8df8fca2cb6
Bob Crocco <bob.crocco@jpl.nasa.gov>
2019-04-02 09:22:08 -0700

    adds a command to build a mesh locally from an a pipeline project (aligned or priors)

---
+++ Pipeline/Commands/LocalBuildMeshes.cs
@@ -0,0 +1,82 @@
+﻿using CommandLine;
+using OPS.Geometry;
+using OPS.Pipeline.MeshWorker;
+using OPS.Util;
+using System;
+using System.IO;
+
+namespace OPS.Pipeline
+{
+    [Verb("local-build-meshes", HelpText = "create mesh locally")]
+    public class LocalBuildMeshesOptions : PipelineCoreOptions
+    {
+        [Value(0, Required = true, HelpText = "project name", Default = null)]
+        public string ProjectName { get; set; }
+
+        [Option(HelpText = "the type of tiling project (currently only MSL supported)", Default = "MSL")]
+        public string ProjectType { get; set; }
+
+        [Option(HelpText = "Output directory, or omit to save to project storage", Default = null)]
+        public string OutputFolder { get; set; }
+
+        [Option(HelpText = "don't build textures for the mesh", Default = true)]
+        public bool NoTextures { get; set; }
+
+        [Option(HelpText = "Allowed source for transform priors: PlacesDB or Landform. PDS is implicit for observations", Default = "PlacesDB")]
+        public string TransformSource { get; set; }
+
+        [Option(HelpText = "Operate on cloud data", Default = false)]
+        public bool Cloud { get; set; }
+    }
+
+    public class LocalBuildMeshes
+    {
+        private LocalBuildMeshesOptions options;
+        private PipelineCore pipeline;
+
+        public LocalBuildMeshes(LocalBuildMeshesOptions options)
+        {
+            this.options = options;
+            if (options.Cloud)
+            {
+                throw new NotImplementedException("building meshes from cloud data not supported yet");
+            }
+            else
+            {
+                this.pipeline = new LocalPipeline(options);
+            }
+
+            if (options.NoTextures == false)
+            {
+                throw new NotImplementedException("texture building not implemented yet");
+            }
+
+            if (options.ProjectType != "MSL")
+            {
+                throw new NotImplementedException("project type not implemented yet");
+            }
+        }
+
+        public int Run()
+        {
+            string outputPath = pipeline.GetLocalDebugFolder(options.OutputFolder,
+                                                             "tiling/" + options.TransformSource,
+                                                             options.ProjectName);
+            PathHelper.EnsureExists(outputPath);
+
+            pipeline.LogInfo("Building full mesh for {0} from {1}", options.ProjectName, options.TransformSource);
+            Mesh mesh = BuildTilingInput.BuildMesh(this.pipeline, options.ProjectName);
+            if(mesh == null)
+            {
+                pipeline.LogError("Mesh building for {0) failed.", options.ProjectName);
+                return 1;
+            }
+
+            string meshFilePath = Path.Combine(outputPath, "fullMesh.ply");
+            pipeline.LogInfo("Saving full mesh to: {0}", meshFilePath);
+            mesh.Save(meshFilePath);
+
+            return 0;
+        }
+    }
+}
