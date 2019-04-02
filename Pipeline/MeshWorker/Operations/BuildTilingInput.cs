3e5332411b6941d5de86292614f4e5b5207fe5e9
Bob Crocco <bob.crocco@jpl.nasa.gov>
2019-04-01 22:27:59 -0700

    extract the mesh building code from the tiliing input and run with a passed in local pipeline

--- Pipeline/MeshWorker/Operations/BuildTilingInput.cs
+++ Pipeline/MeshWorker/Operations/BuildTilingInput.cs
@@ -34,62 +34,7 @@ namespace OPS.Pipeline.MeshWorker
         public int Process()
         {
             pipeline.LogInfo("started");
-
-            var frameCache = new FrameCache(pipeline, projectName);
-            frameCache.Preload(loadTransforms: true);
-            var observationCache = new ObservationCache(pipeline, projectName);
-            observationCache.Preload(obs => obs.UseForReconstruction);
-
-            //temporarily suppress mastcam point cloud data until validated
-            //https://github.jpl.nasa.gov/OnSight/Landform/issues/261
-            var observations = Meshing.CollectMeshObservations(frameCache, observationCache, allowMastcam: false,
-                                                               requireNormals: true);
-            if (observations.Count == 0)
-            {
-                pipeline.LogError("no observations were found to build a point cloud");
-                return 1;
-            }
-            
-            //accumulate the large point cloud
-            Mesh aggregatePointCloud = new Mesh(hasNormals: true);
-            for (int idx = 0; idx < observations.Count; idx++)
-            {
-                var obs = observations[idx];
-                pipeline.LogInfo("building point cloud {0}/{1} ({2})%): {3}", idx + 1, observations.Count,
-                                 (int)(100 * idx / (float)observations.Count), obs.Points.FrameName);
-                var mesh = Meshing.BuildPointCloud(pipeline, obs, frameCache, scaleNormalsByConfidence: true);
-                aggregatePointCloud.MergeWith(new Mesh[] { mesh }, normalize: false);
-            }
-            
-            // build the large mesh from the aggregate point cloud using poisson reconstruction
-            if (aggregatePointCloud.Vertices.Count == 0)
-            {
-                pipeline.LogError("aggregate point cloud contains no points");
-                return 1;
-            }
-          
-            pipeline.LogInfo("reconstructing point cloud: " + aggregatePointCloud.Vertices.Count() + " vertices");
-            PoissonReconstruction.Options opts = new PoissonReconstruction.Options
-            {
-                // suppresses the large wings often seen when extrapolating without orbital data 
-                Boundary = PoissonReconstruction.BoundaryTypes.Dirichlet,
-
-                // no features should be finer than this many meters as this is the finest the octree will dice
-                MinOctreeCellWidthMeters = 0.05f,
-
-                // a value on the upper end of the suggested range in the docs
-                // meaning we think our data in noisy, so wait for this many samples in a cell
-
-                MinOctreeSamplesPerCell = 15,
-                // attempts to allow higher order surfaces than the defaults
-                BSplineDegree = 2,
-
-                // indicates the normal magnitudes are not uniformly unit scaled
-                // to indicate confidence in the position attached to it
-                UseNormalsForConfidence = true
-            };
-
-            Mesh surfacedMesh = PoissonReconstruction.Reconstruct(aggregatePointCloud, opts);            
+            Mesh surfacedMesh = BuildMesh(pipeline, projectName);
             if (surfacedMesh == null || surfacedMesh.Vertices.Count == 0)
             {
                 pipeline.LogError("point cloud failed to reconstruct");
@@ -116,6 +61,65 @@ namespace OPS.Pipeline.MeshWorker
             pipeline.LogInfo("complete");
 
             return 0;
+        }
+
+        static public Mesh BuildMesh(PipelineCore pipeline, string projectName)
+        {
+            var frameCache = new FrameCache(pipeline, projectName);
+            frameCache.Preload(loadTransforms: true);
+            var observationCache = new ObservationCache(pipeline, projectName);
+            observationCache.Preload(obs => obs.UseForReconstruction);
+
+            //temporarily suppress mastcam point cloud data until validated
+            //https://github.jpl.nasa.gov/OnSight/Landform/issues/261
+            var observations = Meshing.CollectMeshObservations(frameCache, observationCache, allowMastcam: false,
+                                                               requireNormals: true);
+            if (observations.Count == 0)
+            {
+                pipeline.LogError("no observations were found to build a point cloud");
+                return null;
+            }
+
+            //accumulate the large point cloud
+            Mesh aggregatePointCloud = new Mesh(hasNormals: true);
+            for (int idx = 0; idx < observations.Count; idx++)
+            {
+                var obs = observations[idx];
+                pipeline.LogInfo("building point cloud {0}/{1} ({2})%): {3}", idx + 1, observations.Count,
+                                 (int)(100 * idx / (float)observations.Count), obs.Points.FrameName);
+                var mesh = Meshing.BuildPointCloud(pipeline, obs, frameCache, scaleNormalsByConfidence: true);
+                aggregatePointCloud.MergeWith(new Mesh[] { mesh }, normalize: false);
+            }
+
+            // build the large mesh from the aggregate point cloud using poisson reconstruction
+            if (aggregatePointCloud.Vertices.Count == 0)
+            {
+                pipeline.LogError("aggregate point cloud contains no points");
+                return null;
+            }
+
+            pipeline.LogInfo("reconstructing point cloud: " + aggregatePointCloud.Vertices.Count() + " vertices");
+            PoissonReconstruction.Options opts = new PoissonReconstruction.Options
+            {
+                // suppresses the large wings often seen when extrapolating without orbital data 
+                Boundary = PoissonReconstruction.BoundaryTypes.Dirichlet,
+
+                // no features should be finer than this many meters as this is the finest the octree will dice
+                MinOctreeCellWidthMeters = 0.05f,
+
+                // a value on the upper end of the suggested range in the docs
+                // meaning we think our data in noisy, so wait for this many samples in a cell
+
+                MinOctreeSamplesPerCell = 15,
+                // attempts to allow higher order surfaces than the defaults
+                BSplineDegree = 2,
+
+                // indicates the normal magnitudes are not uniformly unit scaled
+                // to indicate confidence in the position attached to it
+                UseNormalsForConfidence = true
+            };
+
+            return PoissonReconstruction.Reconstruct(aggregatePointCloud, opts);
         }
     }
 }
