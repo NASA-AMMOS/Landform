f023714742a7a83d7ef788751ee072b9db0de7b4
Alex Menzies <menzies@jpl.nasa.gov>
2019-05-23 16:54:48 -0700

    Updated fetch command to support filtering by sitedrive and/or by a list of product ids.  Also add file size check to prevent partial file downloads

--- Pipeline/Alignment/IngestPDSImage.cs
+++ Pipeline/Alignment/IngestPDSImage.cs
@@ -35,6 +35,21 @@ namespace OPS.Pipeline
             this.mission = MissionSpecific.GetInstance(project.Mission);
         }
 
+        public static string FilenameToSiteDrive(string filename)
+        {
+            RoverProductId id = RoverProductId.ParseFromString(filename);
+            if (id == null)
+            {
+                return null;
+            }
+            if (id.Producer == RoverProductProducer.OPGS)
+            {
+                OPGSProductId opgsId = (OPGSProductId)id;
+                return opgsId.SiteDrive.ToString();
+            }
+            return null;
+        }
+
         /// <summary>
         /// Check if we should even bother reading the header, based on the filename.
         /// </summary>
