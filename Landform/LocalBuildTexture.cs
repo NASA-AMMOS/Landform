a78a1d2c837192c5049cadf68b08a713a57d2469
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-09-19 19:17:29 -0400

    update LocalBuildTexture() to use RunPhase()

--- Landform/LocalBuildTexture.cs
+++ Landform/LocalBuildTexture.cs
@@ -26,14 +26,13 @@ namespace OPS.Landform
 
         [Option(HelpText = "Don't generate index image", Default = false)]
         public bool NoIndex { get; set; }
-
-        [Option(HelpText = "Texture variant (Original, Blurred, Blended)", Default = Backproject.TextureVariant.Original)]
-        public Backproject.TextureVariant TextureVariant { get; set; }
     }
 
     public class LocalBuildTexture : TextureCommand
     {
-        protected new LocalBuildTextureOptions options;
+        private const string OUT_DIR = "texturing/TextureProducts";
+
+        private LocalBuildTextureOptions options;
 
         public LocalBuildTexture(LocalBuildTextureOptions options) : base(options)
         {
@@ -42,14 +41,31 @@ namespace OPS.Landform
 
         public int Run()
         {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+            StartStopwatch();
 
             try
             {
-                if (!ParseArgumentsAndLoadCaches("meshing/TextureProducts"))
+                if (!ParseArgumentsAndLoadCaches())
                 {
                     return 0; //help
+                }
+
+                RunPhase(string.Format("checking/generating {0} observation textures", options.TextureVariant),
+                         EnsureOrGenerateObservationTextures);
+
+                RunPhase("loading input mesh", () => LoadInputMesh(requireUVs: true));
+                RunPhase("build occlusion datastructures", BuildSceneCaster);
+                RunPhase("backproject observations", BackprojectObservations);
+
+                if (!options.NoIndex)
+                {
+                    RunPhase("generate backproject index", () => { GenerateBackprojectIndex(); });
+                }
+
+                if (!options.NoTexture)
+                {
+                    RunPhase(string.Format("generate {0} backproject texture", options.TextureVariant),
+                             () => { GenerateBackprojectTexture(options.TextureVariant); });
                 }
             }
             catch (Exception ex)
@@ -58,128 +74,24 @@ namespace OPS.Landform
                 return 1;
             }
 
-            string what = "";
-            try
-            {
-                what = "observation textures";
-                EnsureOrGenerateObservationTextures();
-
-                what = "input mesh";
-                LoadInputMesh();
-
-                what = "occlusion datastructures";
-                BuildSceneCaster();
-                
-                what = "backprojection";
-                BackprojectObservations();
-
-                if (!options.NoIndex)
-                {
-                    what = "backproject index";
-                    GenerateBackprojectIndex();
-                }
-
-                if (!options.NoTexture)
-                {
-                    what = "backproject texture";
-                    GenerateBackprojectTexture(options.TextureVariant);
-                }
-            }
-            catch (Exception ex)
-            {
-                pipeline.LogError("failed to load or generate {0}: {1}", what, ex.Message);
-                return 1;
-            }
-
-            stopwatch.Stop();
-            pipeline.LogInfo("elapsed time {0:F3}s", 0.001 * stopwatch.ElapsedMilliseconds);
+            StopStopwatch();
 
             return 0;
         }
 
-        private void EnsureOrGenerateObservationTextures()
-        {
-            switch (options.TextureVariant)
-            {
-                case Backproject.TextureVariant.Original: break;
-                case Backproject.TextureVariant.Blurred: GenerateBlurredObservationImages(); break;
-                case Backproject.TextureVariant.Blended: EnsureBlendedObservationImages(); break;
-                default: throw new Exception("unknown texture variant " + options.TextureVariant);
-            }
-        }
-
-        private void EnsureBlendedObservationImages()
-        {
-            foreach (var obs in imageObservations)
-            {
-                if (obs.BlendedGuid == Guid.Empty)
-                {
-                    throw new Exception(string.Format("no blended texture for observation {0}, run local-blend-images"));
-                }
-            }
-        }
-
-        protected override bool ParseArgumentsAndLoadCaches(string outDir)
+        private bool ParseArgumentsAndLoadCaches()
         {
             if (options.NoIndex && options.NoTexture)
             {
                 throw new Exception("cannot specify both --noindex and --notexture");
             }
 
-            if (options.DecimateWedgeImages < 0 || options.DecimateWedgeImages > 1)
-            {
-                throw new Exception("--decimatewedgeimages is not implemented for this command");
-            }
-
-            return base.ParseArgumentsAndLoadCaches(outDir);
+            return base.ParseArgumentsAndLoadCaches(OUT_DIR);
         }
 
-        private void LoadInputMesh()
+        protected override bool ParseArgumentsAndLoadCaches(string outDir)
         {
-            sceneMesh = SceneMesh.Find(pipeline, project.Name, meshFrame);
-
-            if (!string.IsNullOrEmpty(options.InputMesh))
-            {
-                pipeline.LogInfo("loading input mesh from {0}", options.InputMesh);
-                mesh = Mesh.Load(pipeline.GetFileCached(options.InputMesh, "meshes"));
-            }
-            else if (sceneMesh != null)
-            {
-                if (sceneMesh.MeshGuid != Guid.Empty)
-                {
-                    pipeline.LogInfo("loading scene mesh in frame {0} from database", meshFrame);
-                    mesh = pipeline.GetDataProduct<PlyGZDataProduct>(project, sceneMesh.MeshGuid).Mesh;
-                }
-                else
-                {
-                    throw new Exception("scene mesh in database but without mesh");
-                }
-            }
-            else
-            {
-                throw new Exception("no input mesh specified and no scene mesh in database");
-            }
-
-            if (mesh == null)
-            {
-                throw new Exception("failed to load input mesh");
-            }
-
-            if (mesh.Faces.Count == 0)
-            {
-                throw new Exception("input mesh empty");
-            }
-
-            if (!mesh.HasUVs)
-            {
-                throw new Exception("input mesh needs UVs");
-            }
-
-            if (sceneMesh == null)
-            {
-                sceneMesh = SceneMesh.Create(pipeline, project, meshFrame, siteDrives, MeshVariant.Default, mesh,
-                                             noSave: options.NoSave);
-            }
+            throw new NotImplementedException();
         }
     }
 }
