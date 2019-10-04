f5f0c5a6cf9cecd0a2007d4b8582c7579f12266a
Bob Crocco <bob.crocco@jpl.nasa.gov>
2019-10-04 07:39:03 -0700

    option and location for loading lods for meshes

--- Landform/GeometryCommand.cs
+++ Landform/GeometryCommand.cs
@@ -24,6 +24,9 @@ namespace OPS.Landform
     {
         [Option(HelpText = "Scene mesh coordinate frame: auto, passthrough, newest, oldest, mission_root, project_root, numeric sitedrive SSSSSDDDDD", Default = "auto")]
         public virtual string MeshFrame { get; set; }
+
+        [Option(HelpText = "Use level of detail meshes provided in input mesh instead of decimation", Default = false)]
+        public bool LoadLODs { get; set; }
     }
 
     public class GeometryCommand : WedgeCommand
@@ -33,6 +36,7 @@ namespace OPS.Landform
         protected string meshFrame;
 
         protected Mesh mesh;
+        protected List<Mesh> meshLODs;  //the full set of levels of detail for the mesh with the first being highest quality and the last being worst quality
         protected SceneMesh sceneMesh;
 
         public GeometryCommand(GeometryCommandOptions gcopts) : base(gcopts)
