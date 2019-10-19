3c0ef4cacc33f9717501d18f2713c10024f8926a
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-10-18 18:31:33 -0700

    parallelize CleverCombine

--- Geometry/Geometry/CleverCombinePointClouds.cs
+++ Geometry/Geometry/CleverCombinePointClouds.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
-using System.Text.RegularExpressions;
 using Microsoft.Xna.Framework;
 using OPS.Util;
 
@@ -12,10 +12,245 @@ namespace OPS.Geometry
     public class CleverCombinePointClouds
     {
         //settings
-        private const double CellSize = 0.025; //size of cell (meters)
-        private const double MinDistRange = 1.2; //if the max distance is this many times bigger than the minimum, the point with max distance can be pruned
-        private const double SmallestNNDistance = 0.005; //if any point's distance to another is < this number stop searching
-        private const double MaxMSEThreshold = 0.0001; //if a point's error is greater than this it is a candidate for removal
+
+        //size of XY grid cell (meters)
+        private const double CellSize = 0.025;
+
+        //if the max distance from a grid cell to a point cloud origin is this many times bigger than the minimum
+        //the points from that cloud can be pruned from the grid cell
+        private const double MinDistRange = 1.2;
+
+        //max number of random sample points within a grid cell to use for mean squared error computation
+        private const int MaxMSESamples = 30;
+
+        //if any point's distance to another is < this number stop searching for nearest neighbors
+        private const double SmallestNNDistance = 0.005;
+
+        //if the mean squared error between the nearest neighbor samples of the points from a point cloud
+        //within a grid cell to all the other points in the cell is greater than this
+        //then prune the points from that cloud from the cell
+        private const double MaxMSEThreshold = 0.0001;
+
+        /// <summary>
+        /// a weighted combination of redundant point cloud data resulting in a single winner per voxel
+        //  note: all input pointclouds are expected to be in the same reference frame
+        /// </summary>
+        /// <param name="origins">a position from which the distance of each point is a meaningful quality estimate (eg. site drive center, camera origin, etc) </param>
+        /// <param name="clouds">point clouds to combine, order should match pointcloudorigins</param>
+        public static Mesh Combine(Vector3[] origins, Mesh[] clouds, ILogger logger = null)
+        {
+            int numClouds = clouds.Length;
+            if (origins.Length != numClouds)
+            {
+                throw new ArgumentException("number of point clouds must match number of origins");
+            }
+            
+            if (numClouds < 1)
+            {
+                return new Mesh();
+            }
+
+            BoundingBox bbox = clouds[0].Bounds();
+            foreach (var cloud in clouds)
+            {
+                bbox = BoundingBox.CreateMerged(bbox, cloud.Bounds());
+            }
+
+            //XY grid dimensions
+            int width = (int)Math.Ceiling(bbox.Extent().X / CellSize);
+            int height = (int)Math.Ceiling(bbox.Extent().Y / CellSize);
+
+            //collect points into grid cells
+            //grid[i, j][c] = list of indices of points in cloud c in cell (i, j)
+            if (logger != null)
+            {
+                logger.LogInfo("CleverCombine: allocating {0}x{1} grid of {2} {3}x{3}m cells",
+                               width, height, Fmt.KMG(width * height), CellSize);
+            }
+            var grid = new ConcurrentDictionary<int, List<int>>[height, width];
+            CoreLimitedParallel.ForEach(Enumerable.Range(0, width * height), cell =>
+            {
+                int i = cell / width, j = cell % width;
+                grid[i, j] = new ConcurrentDictionary<int, List<int>>();
+            });
+
+            int np = clouds.Sum(cloud => cloud.Vertices.Count);
+
+            if (logger != null)
+            {
+                logger.LogInfo("CleverCombine: gridding {0} points from {1} clouds", Fmt.KMG(np), numClouds);
+            }
+            CoreLimitedParallel.ForEach(Enumerable.Range(0, numClouds), c =>
+            {
+                var verts = clouds[c].Vertices;
+                for (int p = 0; p < verts.Count; p++)
+                {
+                    Vector3 pt = verts[p].Position;
+                    if (bbox.Contains(pt) != ContainmentType.Disjoint)
+                    {
+                        int j = (int)Math.Floor((pt.X - bbox.Min.X) / CellSize);
+                        int i = (int)Math.Floor((pt.Y - bbox.Min.Y) / CellSize);
+                        grid[i, j].AddOrUpdate(c, _ => new List<int>() { p }, (_, l) => { l.Add(p); return l; });
+                    }
+                }
+            });
+
+            //prune points from outlier clouds in each cell
+            if (logger != null)
+            {
+                logger.LogInfo("CleverCombine: pruning {0} cells", Fmt.KMG(width * height));
+            }
+            var keepers = new ConcurrentBag<Vertex>();
+            var rng = NumberHelper.MakeRandomGenerator();
+            CoreLimitedParallel.ForEach(Enumerable.Range(0, width * height), cell =>
+            {
+                int i = cell / width, j = cell % width;
+                double[] cellToCloudOrigin = origins.Select(origin =>
+                {
+                    double dx = origin.X - ((j + 0.5) * CellSize + bbox.Min.X);
+                    double dy = origin.Y - ((i + 0.5) * CellSize + bbox.Min.Y);
+                    return Math.Sqrt(dx * dx + dy * dy);
+                }).ToArray();
+                
+                List<int> cloudsInCell = new List<int>(grid[i, j].Keys);
+                
+                //first filter: remove clouds whose origin is too far from this grid cell
+                while (cloudsInCell.Count > 1)
+                {
+                    int maxDistIdx = -1;
+                    double minDist = double.PositiveInfinity;
+                    double maxDist = double.NegativeInfinity;
+                    
+                    for (int k = 0; k < cloudsInCell.Count; k++)
+                    {
+                        double dist = cellToCloudOrigin[cloudsInCell[k]];
+                        
+                        if (dist < minDist)
+                        {
+                            minDist = dist;
+                        }
+                        
+                        if (dist > maxDist)
+                        {
+                            maxDist = dist;
+                            maxDistIdx = k;
+                        }
+                    }
+                    
+                    if (maxDist > minDist * MinDistRange)
+                    {
+                        cloudsInCell.RemoveAt(maxDistIdx);
+                        continue;
+                    }
+                    
+                    break; //no more outlier clouds
+                }
+                
+                //second filter: remove clouds where a sampling of their points within this cell
+                //is too far from their nearest neighbors in other clouds in this cell
+                while (cloudsInCell.Count > 1)
+                {
+                    double maxMSE = double.NegativeInfinity;
+                    int maxMSECloud = -1;
+                    for (int k = 0; k < cloudsInCell.Count; k++)
+                    {
+                        var cloud = cloudsInCell[k];
+                        var cloudPts = grid[i, j][cloud];
+                        
+                        int[] samples = Enumerable.Range(0, cloudPts.Count)
+                            .OrderBy(x => rng.NextDouble())
+                            .Take(Math.Min(cloudPts.Count, MaxMSESamples))
+                            .Select(idx => cloudPts[idx])
+                            .ToArray();
+                        
+                        double mse = 0;
+                        int numDistances = 0;
+                        for (int l = 0; l < cloudsInCell.Count; l++)
+                        {
+                            if (l != k)
+                            {
+                                var otherCloud = cloudsInCell[l];
+                                var otherCloudPts = grid[i, j][otherCloud];
+                                foreach (var sample in samples)
+                                {
+                                    Vector3 pt = clouds[cloud].Vertices[sample].Position;
+                                    double minDist = double.PositiveInfinity;
+                                    foreach (var otherPtIdx in otherCloudPts)
+                                    {
+                                        Vector3 otherPt = clouds[otherCloud].Vertices[otherPtIdx].Position;
+                                        double dist = Vector3.DistanceSquared(pt, otherPt);
+                                        if (dist < minDist)
+                                        {
+                                            minDist = dist;
+                                        }
+                                        if (dist < SmallestNNDistance)
+                                        {
+                                            break;
+                                        }
+                                    }
+                                    mse += minDist;
+                                    numDistances++;
+                                }
+                            }
+                        }
+                        if (numDistances > 0)
+                        {
+                            mse /= numDistances;
+                        }
+                        if (mse > maxMSE)
+                        {
+                            maxMSE = mse;
+                            maxMSECloud = k;
+                        }
+                    }
+                    
+                    if (maxMSE > MaxMSEThreshold)
+                    {
+                        cloudsInCell.RemoveAt(maxMSECloud);
+                        continue;
+                    }
+                    
+                    break; //no more outlier clouds
+                }
+                
+                foreach (var cloud in cloudsInCell)
+                {
+                    foreach (var ptIdx in grid[i, j][cloud])
+                    {
+                        keepers.Add(new Vertex(clouds[cloud].Vertices[ptIdx]));
+                    }
+                }
+            });
+
+            if (logger != null)
+            {
+                logger.LogInfo("CleverCombine: kept {0} vertices", Fmt.KMG(keepers.Count));
+            }
+
+            bool hasNormals = clouds.Any(pc => pc.HasNormals);
+            bool hasUVs = clouds.Any(pc => pc.HasUVs);
+            bool hasColors = clouds.Any(pc => pc.HasColors);
+            Mesh output = new Mesh(hasNormals, hasUVs, hasColors);
+
+            foreach (var keeper in keepers)
+            {
+                output.Vertices.Add(keeper);
+            }
+
+            if (logger != null)
+            {
+                logger.LogInfo("CleverCombine: removing duplicate vertices");
+            }
+
+            output.RemoveDuplicateVertices();
+
+            if (logger != null)
+            {
+                logger.LogInfo("CleverCombine: returning {0} vertices", Fmt.KMG(output.Vertices.Count));
+            }
+
+            return output;
+        }
 
         /// <summary>
         /// a weighted combination of redundant point cloud data resulting in a single winner per voxel
@@ -23,7 +258,7 @@ namespace OPS.Geometry
         /// </summary>
         /// <param name="inputPointCloudOrigins">a position from which the distance of each point is a meaningful quality estimate (eg. site drive center, camera origin, etc) /param>
         /// <param name="inputPointClouds">point clouds to combine, order should match pointcloudorigins</param>
-        public static Mesh Combine(Vector3[] inputPointCloudOrigins, Mesh[] inputPointClouds)
+        public static Mesh CombineLegacy(Vector3[] inputPointCloudOrigins, Mesh[] inputPointClouds)
         {
             // Compute bounds of surface area
             BoundingBox bbox = inputPointClouds.FirstOrDefault().Bounds();
