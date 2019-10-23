644482e9e6c44ab2e17443d40ec40434befb042d
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-10-23 14:37:31 -0700

    make changes requested by Adrian Tinio

--- Web/docs/build_manual.sh
+++ Web/docs/build_manual.sh
@@ -3,14 +3,24 @@
 # install pandoc from https://pandoc.org/installing.html
 # install texlive from http://www.tug.org/texlive/acquire-netinstall.html
 
-sections="INTRODUCTION.md SETUP.md API.md TEST.md"
-filename=Landform_Mesh_Tiling_Server
-title="Landform Mesh Tiling Server"
+# Note from Adrian Tinio: MGSS specifically requests we use the name "Geometry Streaming Server Product Guide"
 
-pandoc $sections -o $filename.html -s --toc --metadata pagetitle="$title" --css pandoc.css -V title="$title"
-pandoc $sections -o $filename.pdf -s --toc --metadata pagetitle="$title" --css pandoc.css -V title="$title"
+sections="INTRODUCTION.md SETUP.md API.md TEST.md"
+filename=Geometry_Streaming_Server_Product_Guide
+title="Geometry Streaming Server Product Guide"
+
+# vertical bars in tables hack
+# https://gist.github.com/svenevs/41a1a434a055adcee56bd1f0374fa254
+python=py
+
+pandoc $sections -s -o $filename.html --toc --metadata pagetitle="$title" --css pandoc.css -V title="$title"
+pandoc $sections -s -f markdown -t latex --template latex.template --toc --metadata pagetitle="$title" --css pandoc.css -V title="$title" | $python tex_table_verts.py > $filename.tex
+pdflatex $filename.tex
+pdflatex $filename.tex
 
 for s in $sections; do
   s=${s%.*}
-  pandoc $s.md -o $s.pdf -s --toc --metadata pagetitle="$title $s" --css pandoc.css -V title="$title $s"
+  pandoc $s.md -s -f markdown -t latex --template latex.template --toc --metadata pagetitle="$title $s" --css pandoc.css -V title="$title $s" | $python tex_table_verts.py > $s.tex
+  pdflatex $s.tex
+  pdflatex $s.tex
 done
