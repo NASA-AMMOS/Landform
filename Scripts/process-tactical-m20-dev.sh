d09c0880c4f927e647743778d5c895ea1f1fcdf1
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-11-21 13:30:37 -0800

    tweak scripts

--- Scripts/process-tactical-m20-dev.sh
+++ Scripts/process-tactical-m20-dev.sh
@@ -7,4 +7,5 @@ dir=./Landform/bin/Release
 logdir=`pwd`/log
 tmpdir=`pwd`/tmp
 
+$dir/Landform.exe configure-local --venue=local --storagedir=c:/Users/$USERNAME/Documents/landform-storage --maxcores=0 --randomseed=-1
 $dir/Landform.exe process-tactical --service --mission M2020 --queuename $queue --failqueuename $failqueue --meshformat obj --logdir $logdir --tempdir $tmpdir --stacktraces
--- Scripts/process-tactical-m20-ec2.bat
+++ Scripts/process-tactical-m20-ec2.bat
@@ -5,4 +5,5 @@ set dir=c:\landform\Landform-%ver%
 set logdir=c:\log\landform-tactical
 set tmpdir=c:\temp\landform-tactical
 
-%dir%\Landform.exe process-tactical --service --mission M2020 --queuename %queue% --failqueuename %failqueue% --awsprofile none --awsregion none --meshformat obj --logdir %logdir% --tempdir %tmpdir% --stacktraces
+%dir%\Landform.exe configure-local --venue=local --storagedir=c:\Users\%USERNAME%\Documents\landform-storage --maxcores=0 --randomseed=-1 > nul 2> nul
+%dir%\Landform.exe process-tactical --service --mission M2020 --queuename %queue% --failqueuename %failqueue% --awsprofile none --awsregion none --meshformat obj --logdir %logdir% --tempdir %tmpdir% --stacktraces > nul 2> nul
