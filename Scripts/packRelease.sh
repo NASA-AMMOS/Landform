3c03cc998ea2837a33245f1b9c337fd1610539d0
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-11-27 08:46:23 -0800

    pack TilingServer.exe and LandformUtil.exe with release

--- Scripts/packRelease.sh
+++ Scripts/packRelease.sh
@@ -7,29 +7,19 @@ fi
 
 ver=$1
 dir=Landform-$ver
-src=Landform/bin/Release
 zf=Landform-$ver.zip
 
 echo "clearing output directory $dir"
 rm -rf $dir
 mkdir $dir
 
-echo "copying $src to $dir"
-cp -R $src/* $dir
+for src in TilingServer/bin/Release LandformUtil/bin/Release Landform/bin/Release Dependencies Scripts Utils; do
+    echo "copying $src to $dir"
+    cp -R $src/* $dir
+done
+
 rm -rf $dir/log
 rm -rf $dir/tmp
-
-src=Dependencies
-echo "copying $src to $dir"
-cp -R $src/* $dir
-
-src=Scripts
-echo "copying $src to $dir"
-cp -R $src/* $dir
-
-src=Utils
-echo "copying $src to $dir"
-cp -R $src/* $dir
 
 echo "zipping $zf"
 rm -f $zf
