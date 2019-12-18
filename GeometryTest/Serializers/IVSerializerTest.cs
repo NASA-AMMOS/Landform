a524a2c2fb01a5f8bd7cb8880f6ab92031718846
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2019-12-18 17:29:02 -0500

    bugfix unit test

--- GeometryTest/Serializers/IVSerializerTest.cs
+++ GeometryTest/Serializers/IVSerializerTest.cs
@@ -14,25 +14,34 @@ namespace GeometryThirdpartyTest
         public void OpenInventorReadTest()
         {
             string filepath = Path.Combine("TestData", "mesh", "MLF_474203921RASLS0450000MCAM03804M1.iv");
+
             var meshes = (new IVSerializer()).LoadAllLODs(filepath);
-            Assert.AreEqual(2, meshes.Count);
-            Assert.AreEqual(3175, meshes[0].Vertices.Count);
-            Assert.AreEqual(6784, meshes[0].Faces.Count);
+            Assert.AreEqual(3, meshes.Count);
+            Assert.AreEqual(2980, meshes[0].Vertices.Count);
+            Assert.AreEqual(5289, meshes[0].Faces.Count);
             Assert.IsTrue(meshes[0].HasUVs);
             Assert.IsFalse(meshes[0].HasColors);
-            Assert.IsFalse(meshes[0].HasNormals);
-            Assert.AreEqual(318, meshes[1].Vertices.Count);
-            Assert.AreEqual(392, meshes[1].Faces.Count);
+            Assert.IsTrue(meshes[0].HasNormals);
+
+            Assert.AreEqual(959, meshes[1].Vertices.Count);
+            Assert.AreEqual(1497, meshes[1].Faces.Count);
             Assert.IsTrue(meshes[1].HasUVs);
             Assert.IsFalse(meshes[1].HasColors);
-            Assert.IsFalse(meshes[1].HasNormals);
+            Assert.IsTrue(meshes[1].HasNormals);
+
+            Assert.AreEqual(318, meshes[2].Vertices.Count);
+            Assert.AreEqual(392, meshes[2].Faces.Count);
+            Assert.IsTrue(meshes[2].HasUVs);
+            Assert.IsFalse(meshes[2].HasColors);
+            Assert.IsTrue(meshes[2].HasNormals);
 
             var m = (new IVSerializer()).Load(filepath);
-            Assert.AreEqual(3175, m.Vertices.Count);
-            Assert.AreEqual(6784, m.Faces.Count);
+            Assert.AreEqual(2980, m.Vertices.Count);
+            Assert.AreEqual(5289, m.Faces.Count);
             Assert.IsTrue(m.HasUVs);
             Assert.IsFalse(m.HasColors);
-            Assert.IsFalse(m.HasNormals);
+            Assert.IsTrue(m.HasNormals);
+
             m.Save("test.obj");
         }
     }
