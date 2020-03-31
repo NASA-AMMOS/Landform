e5121853eb6743a1a69f2572183f0c8a5c88f913
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2020-03-31 17:11:28 -0400

    add Stamped<T> wrapper class

--- Util/UTCTime.cs
+++ Util/UTCTime.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -19,6 +20,36 @@ namespace OPS.Util
         public static double NowMS()
         {
             return SinceEpoch().TotalMilliseconds;
+        }
+    }
+
+    public class Stamped<T> where T : new()
+    {
+        public readonly T Value;
+        
+        private long _timestamp; //ms since UTC epoch
+        public long Timestamp { get { return Interlocked.Read(ref _timestamp); } }
+        
+        public Stamped(T obj, long now)
+        {
+            this.Value = obj;
+            this._timestamp = now;
+        }
+        
+        public Stamped(long now) : this(new T(), now) { }
+        
+        public Stamped(T obj) : this(obj, (long)UTCTime.NowMS()) { }
+        
+        public Stamped() : this(new T()) { }
+        
+        public void Touch(long now)
+        {
+            Interlocked.Exchange(ref _timestamp, now);
+        }
+        
+        public void Touch()
+        {
+            Touch((long)UTCTime.NowMS());
         }
     }
 }
