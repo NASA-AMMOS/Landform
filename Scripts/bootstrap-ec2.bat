7c73914eb541c9ff1e1232835d282ce32b9a21c7
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2020-08-21 12:42:37 -0400

    add bootstrap script

---
+++ Scripts/bootstrap-ec2.bat
@@ -0,0 +1,37 @@
+@echo off
+
+rem downloads Landform-VERSION.zip from s3://BUCKET/[FOLDER] and unzips it to c:\landform\Landform-VERSION\
+
+set ddir=c:\download
+set lfdir=c:\landform
+
+set lfver=
+if not "%LANDFORM_VERSION%"=="" set lfver=%LANDFORM_VERSION%
+if "%lfver%"=="" (
+   echo LANDFORM_VERSION not set, skipping bootstrap
+   goto:eof
+)
+
+set lfbucket=
+if not "%LANDFORM_DIST_BUCKET%"=="" set lfbucket=%LANDFORM_DIST_BUCKET%
+if "%lfbucket%"=="" (
+   echo LANDFORM_DIST_BUCKET not set, skipping bootstrap
+   goto:eof
+)
+
+set lffolder=
+if not "%LANDFORM_DIST_FOLDER%"=="" set lffolder=%LANDFORM_DIST_FOLDER%
+
+set awsprofile=
+if not "%LANDFORM_AWS_PROFILE%"=="" set awsprofile=--profile=%LANDFORM_AWS_PROFILE%
+
+set awsregion=--region=us-gov-west-1
+if not "%LANDFORM_AWS_REGION%"=="" set awsregion=--region=%LANDFORM_AWS_REGION%
+
+set zip=Landform-%lfver%.zip
+
+aws %awsprofile% %awsregion% s3 cp s3://%lfbucket%/%lffolder%%zip% %ddir%\%zip%
+
+powershell %ddir%\deltree.ps1 %lfdir%
+
+powershell %ddir%\unzip.ps1 %ddir%\%zip% %lfdir%
