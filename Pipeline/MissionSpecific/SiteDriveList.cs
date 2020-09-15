f29a5ffb79440588636e8f09986e3ce8882f7f67
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2020-09-15 15:39:11 -0400

    bugfix YEAR/DOY path regex again

--- Pipeline/MissionSpecific/SiteDriveList.cs
+++ Pipeline/MissionSpecific/SiteDriveList.cs
@@ -11,7 +11,7 @@ namespace OPS.Pipeline
 {
     public class SiteDriveList
     {
-        private static readonly Regex YEAR_DOY_REGEX = new Regex(@"/(\d{4})/\d{3}/");
+        private static readonly Regex YEAR_DOY_REGEX = new Regex(@"/([0-9]{4})/[^/]+/");
 
         public readonly Dictionary<RoverProductId, string> IDToURL = new Dictionary<RoverProductId, string>();
 
