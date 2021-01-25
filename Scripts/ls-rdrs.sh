99e95ddb6155814c38a0f1f451ebfcb83e04d08d
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2021-01-25 18:54:00 -0500

    improvements to ls-rdrs.sh and ls-tilesets.sh

--- Scripts/ls-rdrs.sh
+++ Scripts/ls-rdrs.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# list contextual mesh RDRs in date order
+# url=s3://m20-ids-g-data-g66bt/ods/dev/sol 
+# ./Scripts/ls-rdrs.sh dateorder $url 'L?_[^.]+XYZ_|L?_[^.]+MXY_|L?_[^.]+UVW_|RAS' IMG
+
 if [ $# -lt 1 ]; then
-    echo "USAGE: ls-rdrs.sh s3://bucket/path[/prefix] [RAS|MXY|... [IMG|VIC|IV|...]]"
+    echo "USAGE: ls-rdrs.sh [dateorder] s3://bucket/path[/prefix] [RAS|MXY|... [IMG|VIC|IV|...]]"
     exit 1
 fi
+
+while [[ $# -gt 1 ]]; do
+    case $1 in
+        "dateorder") sortorder="--key=2"; shift;;
+        *) break;;
+    esac
+done
 
 url=$1
 
@@ -20,19 +31,26 @@ else
     exts="IMG|VIC|IV|OBJ|MTL|PNG|TAR"
 fi
 
+ignore="/(ids-pipeline|mesh|browse)/|/ICM-"
+
 s3ls="aws --profile=credss-default s3 ls"
 
 declare -A results
 
+export TZ=utc
+
 while read line; do
     words=($line)
+    date=${words[0]}
+    time=${words[1]}
+    size=${words[2]}
     path=${words[3]}
     file=${path##*/}
     id=${file%.*}
     id=${id%_tileset}
     id=${id%_scene}
     ext=${file##*.}
-    results[$id]="${results[$id]} ${ext}"
-done < <($s3ls $url --recursive | grep -E -i ".*(${products}).*\.(${exts})" | tr -d '\r')
+    results[$id]="${results[$id]} ${ext}:${date};${time}UTC;${size}"
+done < <($s3ls $url --recursive | grep -E -i "($products)[^.]+\.($exts)" | grep -E -i -v "$ignore" | tr -d '\r')
 
-sort <(for id in "${!results[@]}"; do echo "$id:${results[$id]}"; done)
+sort $sortorder <(for id in "${!results[@]}"; do echo "$id${results[$id]}"; done)
--- Scripts/ls-tilesets.sh
+++ Scripts/ls-tilesets.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "USAGE: ls-tilesets.sh s3://bucket/path/rdr/tileset "
+    echo "USAGE: ls-tilesets.sh [verbose] [with-scenes] s3://bucket/path/rdr/tileset"
     exit 1
 fi
+
+while [[ $# -gt 1 ]]; do
+    case $1 in
+        "verbose") verbose=true; shift;;
+        "with-scenes") withscenes=true; shift;;
+        *) break;;
+    esac
+done
 
 url=$1
 
@@ -12,8 +20,16 @@ bucket=${tmp%%/*}
 
 s3ls="aws --profile=credss-default s3 ls"
 
+pattern=tileset.json
+if [ $withscenes ]; then pattern="scene.json|$pattern"; fi
+
+export TZ=utc
+
 while read line; do
     words=($line)
+    date=${words[0]}
+    time=${words[1]}
     path=${words[3]}
+    if [ $verbose ]; then echo -n "${date} ${time}UTC "; fi
     echo s3://$bucket/$path
-done < <($s3ls $url --recursive | grep -E -i "tileset.json" | tr -d '\r')
+done < <($s3ls $url --recursive | grep -E -i "$pattern" | tr -d '\r')
