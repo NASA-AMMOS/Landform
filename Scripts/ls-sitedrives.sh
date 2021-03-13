403df5baef0cd54d71101df4dd1bb272eaa7a422
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2021-03-13 13:07:53 -0500

    add mission option to ls-sitedrives.sh

--- Scripts/ls-sitedrives.sh
+++ Scripts/ls-sitedrives.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "USAGE: ls-sitedrives.sh s3://bucket/path[/prefix]"
+    echo "USAGE: ls-sitedrives.sh s3://bucket/path[/prefix] [M2020|MSL]"
     exit 1
+fi
+
+mission=M2020
+if [[ $# -gt 1 ]]; then
+    mission=$2
+fi
+
+types="RAS_|XYZ_"
+range=29-35
+if [[ "$mission" = "MSL" ]]; then
+    range=19-25
+    types="RAS|XYZ"
 fi
 
 # https://stackoverflow.com/a/246128
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-$scriptdir/ls-rdrs.sh $1 "RAS_|XYZ_" IMG | cut -c29-35 | grep -E "^[0-9]+$" | sort | uniq | paste -sd ","
+$scriptdir/ls-rdrs.sh $1 $types IMG | cut -c${range} | grep -E "^[0-9]+$" | sort | uniq | paste -sd ","
