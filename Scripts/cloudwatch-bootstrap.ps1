6de06dabf5c408b77a09b7ca808e601b481b2a7c
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2022-03-05 16:31:15 -0500

    add config scripts to match new AMI

---
+++ Scripts/cloudwatch-bootstrap.ps1
@@ -0,0 +1,2 @@
+& "C:\Program Files\Amazon\AmazonCloudWatchAgent\amazon-cloudwatch-agent-ctl.ps1" -a fetch-config -m ec2 -s -c file:C:\landform\config_files\amazon-cloudwatch-agent.json
+Start-Sleep -s 30
