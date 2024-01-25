dcd17bca05b2cf22bf7cdfcb8e34187c3e928ec0
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2024-01-25 15:35:55 -0800

    bugfix

--- Landform/ProcessContextual.cs
+++ Landform/ProcessContextual.cs
@@ -2048,11 +2048,15 @@ namespace JPLOPS.Landform
                                            msg.orbitalOnly ? "_orbital" : "");
             string url = $"{destDir}/{project}/";
             pipeline.LogVerbose("checking for tileset under {0}", url);
-            bool absent = true, done = false;
+            bool absent = true, sameMsg = false, hasTileset = false;
             foreach (var f in SearchFiles(url, recursive: false))
             {
                 absent = false;
-                if (f.EndsWith(MESSAGE_JSON))
+                if (f.EndsWith(TILESET_JSON))
+                {
+                    hasTileset = true;
+                }
+                else if (f.EndsWith(MESSAGE_JSON))
                 {
                     try
                     {
@@ -2060,7 +2064,7 @@ namespace JPLOPS.Landform
                         var existingMsg = JsonHelper.FromJson<ContextualMeshMessage>(existingJson);
                         if (existingMsg.SameTileset(msg, pipeline))
                         {
-                            done = true; //unless we also see a matching PID
+                            sameMsg = true;
                         }
                     }
                     catch (Exception ex)
@@ -2096,7 +2100,7 @@ namespace JPLOPS.Landform
                     }
                 }
             }
-            return done ? TilesetStatus.done : absent ? TilesetStatus.absent : TilesetStatus.found;
+            return (sameMsg && hasTileset) ? TilesetStatus.done : absent ? TilesetStatus.absent : TilesetStatus.found;
         }
 
         private class ContextualPIDContent : ServicePIDContent
