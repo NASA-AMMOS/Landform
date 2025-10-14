f414652e44c50f349f7cb72b51bd89cd8c90faf0
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2025-10-14 14:13:43 -0400

    add GeometryThirdPartyTest

---
+++ src/GeometryThirdpartyTest/FSSRTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using JPLOPS.Geometry;
+using JPLOPS.Util;
+
+namespace GeometryThirdpartyTest
+{
+    public class FSSRTest
+    {
+        [Fact]
+        public void FSSRReconstruct()
+        {
+            Mesh m = TestMeshCreator.CreateMesh(true, false, false);
+            m.Faces = new List<Face>();
+            Mesh r = FSSR.Reconstruct(m);
+            r.Save("fssr_recon.ply");
+            Assert.NotEmpty(r.Vertices);
+            Assert.NotEmpty(r.Faces);
+            Assert.True(m.HasNormals);
+            Assert.False(m.HasUVs);
+            Assert.False(m.HasColors);
+        }
+    }
+}
---
+++ src/GeometryThirdpartyTest/UVAtlasTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using JPLOPS.Geometry;
+
+namespace GeometryThirdpartyTest
+{
+    public class UVAtlasTest
+    {
+        [Fact]
+        public void AtlasTest()
+        {
+            Triangle t1 = new Triangle(new Vertex(0, 0, 0), new Vertex(0, 1, 0), new Vertex(1, 0, 0));
+            Triangle t2 = new Triangle(new Vertex(1, 0, 0), new Vertex(0, 1, 0), new Vertex(1, 1, 1));
+            Mesh mesh = new Mesh(new List<Triangle> { t1, t2 });
+            int numNonzeroUV = 0;
+            foreach(Triangle t in mesh.Triangles())
+            {
+                foreach(Vertex v in t.Vertices())
+                {
+                    if (v.UV.X > 0 || v.UV.Y > 0)
+                    {
+                        numNonzeroUV++;
+                    }
+                }
+            }
+            Assert.True(numNonzeroUV == 0);
+            Assert.True(UVAtlas.Atlas(mesh, 512, 512));
+            numNonzeroUV = 0;
+            foreach(Triangle t in mesh.Triangles())
+            {
+                foreach(Vertex v in t.Vertices())
+                {
+                    if (v.UV.X > 0 || v.UV.Y > 0)
+                    {
+                        numNonzeroUV++;
+                    }
+                    Assert.True(v.UV.X >= 0);
+                    Assert.True(v.UV.X <= 1);
+                    Assert.True(v.UV.Y >= 0);
+                    Assert.True(v.UV.Y <= 1);
+                }
+            }
+            Assert.True(numNonzeroUV > 0);
+        }
+    }
+}
