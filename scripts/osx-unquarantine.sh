7ce898944c3ad773956c155deb9217a7c7bc2273
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2025-11-10 10:00:25 -0500

    add uvatlas dylibs to unquarantine

--- scripts/osx-unquarantine.sh
+++ scripts/osx-unquarantine.sh
@@ -8,6 +8,6 @@ cd "$rootdir"
 [[ -d dist ]] && cd dist
 [[ -d src ]] && cd src
 
-for f in fssrecon meshclean PoissonRecon.V13.72 SurfaceTrimmer.V13.72; do
+for f in fssrecon meshclean PoissonRecon.V13.72 SurfaceTrimmer.V13.72 libUVAtlas.dylib libUVAtlasWrapper.dylib; do
   xattr -dr com.apple.quarantine `pwd`/Landform/bin/Release/net9.0/$f
 done
