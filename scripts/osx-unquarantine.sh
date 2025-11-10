9870b234cad5aa8b1ba423e7024189ab52fa4a44
Marsette Vona <marsette.a.vona@jpl.nasa.gov>
2025-11-10 10:27:06 -0500

    moar unquarantine

--- scripts/osx-unquarantine.sh
+++ scripts/osx-unquarantine.sh
@@ -8,6 +8,13 @@ cd "$rootdir"
 [[ -d dist ]] && cd dist
 [[ -d src ]] && cd src
 
+parent=`pwd`
+
 for f in fssrecon meshclean PoissonRecon.V13.72 SurfaceTrimmer.V13.72 libUVAtlas.dylib libUVAtlasWrapper.dylib; do
-  xattr -dr com.apple.quarantine `pwd`/Landform/bin/Release/net9.0/$f
+  for d in Landform Pipeline GeometryThirdParty PipelineTest GeometryThirdPartyTest; do
+    if [[ -f "$parent/$d/bin/Release/net9.0/$f" ]]; then
+      echo "unquarantine $parent/$d/bin/Release/net9.0/$f"
+      xattr -dr com.apple.quarantine "$parent/$d/bin/Release/net9.0/$f"
+    fi
+  done
 done
